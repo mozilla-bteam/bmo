aa11d5da0b9d31fbfb14aa05ec51565ba733d5ca
Dylan William Hardison <dylan@hardison.net>
2015-10-22 12:51:08 -0400

    Revert "Bug 1216300 - Test sentry error logging"
    This reverts commit 4e3aa0dfd94e75450a4b3ea586767686a418b3d5.

--- Bugzilla/Error.pm
+++ Bugzilla/Error.pm
@@ -182,7 +182,7 @@ sub ThrowUserError {
 }
 
 sub ThrowCodeError {
-    my ($error, $vars) = @_;
+    my (undef, $vars) = @_;
 
     # Don't show function arguments, in case they contain
     # confidential data.
@@ -191,7 +191,6 @@ sub ThrowCodeError {
     # as coming from the caller.
     local $Carp::CarpInternal{'Bugzilla::Error'} = 1;
     $vars->{traceback} = Carp::longmess();
-    warn "CodeError[$error]: $vars->{traceback}";
 
     _throw_error("global/code-error.html.tmpl", @_);
 }
