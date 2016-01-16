f9833ba5852a8c2c627b8ef0b34cf5da1af45d37
Dylan Hardison <dylan@mozilla.com>
2016-01-15 22:01:01 -0500

    Revert "Bug 1236161 - when converting a BMP attachment to PNG fails a zero byte attachment is created"
    This reverts commit 2846c1e7d484047334bd7e38bc2e807e986cba44.

--- extensions/BmpConvert/Extension.pm
+++ extensions/BmpConvert/Extension.pm
@@ -25,81 +25,33 @@ use strict;
 use base qw(Bugzilla::Extension);
 
 use Image::Magick;
-use File::Temp qw(:seekable);
-use File::stat qw(stat);
 
 our $VERSION = '1.0';
 
 sub attachment_process_data {
     my ($self, $args) = @_;
-
     return unless $args->{attributes}->{mimetype} eq 'image/bmp';
-    eval {
-        _try_convert_bmp_to_png($args);
-    };
-    warn $@ if $@;
-}
-
-
-# Here be dragons:
-# Image::Magick uses dualvars extensively to signal errors.
-# The documentation is either confusing or wrong in this regard.
-# This is not a great practice. dualvar(0, "foo") is a true value,
-# but dualvar(0, "foo") + 0 is not.
-# Also dualvar(1, "") is a false value, but dualvar(1, "") > 0 is true.
-#
-# "When a scalar has both string and numeric components (dualvars), Perl
-# prefers to check the string component for boolean truth."
-# From https://github.com/chromatic/modern_perl_book/blob/master/sections/coercion.pod
-sub _try_convert_bmp_to_png {
-    my ($args) = @_;
 
     my $data = ${$args->{data}};
     my $img = Image::Magick->new(magick => 'bmp');
-    my $size;
 
+    # $data is a filehandle.
     if (ref $data) {
-        my $read_error = $img->Read(file => \*$data);
-
-        # rewind so it can be read in again by other code
-        seek($data, 0, SEEK_SET);
-
-        die "Error reading in BMP: $read_error"
-          if $read_error;
-
+        $img->Read(file => \*$data);
         $img->set(magick => 'png');
-
-        my $tmp = File::Temp->new(UNLINK => 1, SUFFIX => '.png');
-        my $write_error = $img->Write(file => $tmp);
-
-        die "Error converting BMP to PNG: $write_error"
-          if $write_error;
-
-        $size = stat($tmp->filename)->size;
-        die "Error converting BMP to PNG results in empty file"
-          if $size == 0;
-
-        $tmp->seek(0, SEEK_SET);
-        $data = $tmp;
+        $img->Write(file => \*$data);
     }
+    # $data is a blob.
     else {
-        my $parse_error = $img->BlobToImage($data);
-        die "Error parsing in BMP: $parse_error"
-          if $parse_error;
-
+        $img->BlobToImage($data);
         $img->set(magick => 'png');
         $data = $img->ImageToBlob();
-
-        die "Error converting BMP to PNG (empty PNG)"
-          unless length($data) > 0;
-
-        $size = length($data);
     }
+    undef $img;
 
     ${$args->{data}} = $data;
     $args->{attributes}->{mimetype} = 'image/png';
-    $args->{attributes}->{filename} =~ s/\.bmp$/.png/i;
-    $args->{attributes}->{attach_size} = $size;
+    $args->{attributes}->{filename} =~ s/^(.+)\.bmp$/$1.png/i;
 }
 
  __PACKAGE__->NAME;
