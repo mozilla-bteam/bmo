982d0fd32b97d6f15186ca26928820b17bc507f8
Dylan William Hardison <dylan@hardison.net>
2016-08-19 20:52:24 -0400

    wait longer

--- docker/bugzilla_config.sh
+++ docker/bugzilla_config.sh
@@ -4,7 +4,7 @@ cd $BUGZILLA_ROOT
 
 # Configure database
 /usr/bin/mysqld_safe &
-sleep 10
+sleep 30
 mysql -u root mysql -e "GRANT ALL PRIVILEGES ON *.* TO bugs@localhost IDENTIFIED BY 'bugs'; FLUSH PRIVILEGES;" || exit 1
 mysql -u root mysql -e "CREATE DATABASE bugs CHARACTER SET = 'utf8';" || exit 1
 
