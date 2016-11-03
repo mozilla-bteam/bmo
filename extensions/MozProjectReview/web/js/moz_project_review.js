f29ede4bc91d777680ff7d857a124d8460b318ac
David Lawrence <dkl@mozilla.com>
2016-11-03 22:40:13 +0000

    Bug 1264821 - We want to replace the project kick-off form with a contract request form

--- extensions/MozProjectReview/web/js/moz_project_review.js
+++ extensions/MozProjectReview/web/js/moz_project_review.js
@@ -242,7 +242,7 @@ $(function() {
                              $('#other_party').val());
 
         var component_map = {
-            "Engaging a new vendor Company,": "Vendor/Services",
+            "Engaging a new vendor company,": "Vendor/Services",
             "Adding a new SOW with a vendor": "Vendor/Services",
             "Extending a SOW or renewing a contract": "Vendor/Services",
             "Purchasing hardware": "Vendor/Services",
