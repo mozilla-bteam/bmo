69d54fc94f6096d7f74fb8369ae5536457416f69
David Lawrence <dkl@mozilla.com>
2017-06-19 21:27:24 +0000

    Perl tidied the new modules Some refactoring such as moving attachment creation to a separate function Use phab-bot user for making bug changes Other misc cleanups

--- extensions/PhabBugz/lib/WebService.pm
+++ extensions/PhabBugz/lib/WebService.pm
@@ -17,88 +17,99 @@ use Bugzilla::Attachment;
 use Bugzilla::Bug;
 use Bugzilla::BugMail;
 use Bugzilla::Error;
+use Bugzilla::Group;
 use Bugzilla::User;
 use Bugzilla::Util qw(correct_urlbase detaint_natural);
 use Bugzilla::WebService::Constants;
 
+use Bugzilla::Extension::PhabBugz::Constants;
 use Bugzilla::Extension::PhabBugz::Util qw(
-    create_revision_attachment
-    create_private_revision_policy
-    edit_revision_policy
-    get_bug_role_phids
-    get_project_phid
-    get_revision_by_id
-    intersect
-    make_revision_public
-    request
+  create_revision_attachment
+  create_private_revision_policy
+  edit_revision_policy
+  get_bug_role_phids
+  get_project_phid
+  get_revision_by_id
+  intersect
+  make_revision_public
+  request
 );
 
 use Data::Dumper;
 
 use constant PUBLIC_METHODS => qw(
-    revision
+  revision
 );
 
 sub revision {
-    my ($self, $params) = @_;
+    my ( $self, $params ) = @_;
 
-    unless (defined $params->{revision} && detaint_natural($params->{revision})) {
-        ThrowCodeError('param_required', { param => 'revision' })
+    unless ( defined $params->{revision}
+        && detaint_natural( $params->{revision} ) )
+    {
+        ThrowCodeError( 'param_required', { param => 'revision' } );
     }
-
-    my $user = Bugzilla->set_user(Bugzilla::User->new({ name => 'conduit@mozilla.bugs' }));
 
     # Obtain more information about the revision from Phabricator
     my $revision_id = $params->{revision};
-    my $revision = get_revision_by_id($revision_id);
+    my $revision    = get_revision_by_id($revision_id);
 
     my $revision_phid  = $revision->{phid};
     my $revision_title = $revision->{fields}{title} || 'Unknown Description';
     my $bug_id         = $revision->{fields}{'bugzilla.bug-id'};
 
-    my $bug = Bugzilla::Bug->check($bug_id);
+    my $user = Bugzilla::User->new( { name => AUTOMATION_USER } );
+    Bugzilla->set_user($user);
+
+    my $bug = Bugzilla::Bug->new($bug_id);
 
     # If bug is public then remove privacy policy
     my $result;
-    if (!@{ $bug->groups_in }) {
+    if ( !@{ $bug->groups_in } ) {
         $result = make_revision_public($revision_id);
     }
+
     # Else bug is private
     else {
-        my $phab_sync_groups = Bugzilla->params->{phabricator_sync_groups}
-            || ThrowUserError('invalid_phabricator_sync_groups');
-        my $sync_group_names = [ split('[,\s]+', $phab_sync_groups) ];
+        my $phab_sync_groups = Bugzilla->params->{phabricator_sync_groups};
+        ThrowUserError('invalid_phabricator_sync_groups')
+          unless $phab_sync_groups;
+        my $sync_group_names = [ split( '[,\s]+', $phab_sync_groups ) ];
 
-        my $bug_groups = $bug->groups_in;
-        my $bug_group_names = [ map { $_->name } @$bug_groups ];
+        my $bug_group_names = [ map { $_->name } @{ $bug->groups_in } ];
 
-        my @set_groups = intersect($bug_group_names, $sync_group_names);
+        my @set_groups = intersect( $bug_group_names, $sync_group_names );
 
         # If bug privacy groups do not have any matching synchronized groups,
         # then leave revision private and it will have be dealt with manually.
-        if (!@set_groups) {
+        if ( !@set_groups ) {
             ThrowUserError('invalid_phabricator_sync_groups');
         }
 
-        my $policy_phid = create_private_revision_policy($bug, \@set_groups);
+        my $policy_phid = create_private_revision_policy( $bug, \@set_groups );
         my $subscribers = get_bug_role_phids($bug);
-        $result = edit_revision_policy($revision_phid, $policy_phid, $subscribers);
+        $result =
+          edit_revision_policy( $revision_phid, $policy_phid, $subscribers );
     }
 
-    my $attachment = create_revision_attachment($bug, $revision_id, $revision_title);
+    my $attachment =
+      create_revision_attachment( $bug, $revision_id, $revision_title );
 
-    Bugzilla::BugMail::Send($bug_id, { changer => $user });
+    Bugzilla::BugMail::Send( $bug_id, { changer => Bugzilla->user } );
 
     return {
         result          => $result,
         attachment_id   => $attachment->id,
-        attachment_link => correct_urlbase() . "attachment.cgi?id=" . $attachment->id
+        attachment_link => correct_urlbase()
+          . "attachment.cgi?id="
+          . $attachment->id
     };
 }
 
 sub rest_resources {
     return [
-        qr{^/phabbugz/revision/([^/]+)$}, {
+        qr{^/phabbugz/revision/([^/]+)$},
+        {
             POST => {
                 method => 'revision',
                 params => sub {
