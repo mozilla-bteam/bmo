474a2376bd5060150938acb6a68340bda6c0c0ac
Sebastin Santy <sebastinssanty@gmail.com>
2017-06-24 02:44:11 +0530

    Bug 1365345 - Modal Bug Entry Prototype

--- extensions/BugModal/web/new_bug.js
+++ extensions/BugModal/web/new_bug.js
@@ -16,15 +16,18 @@ $(document).ready(function() {
 
     $('#component').empty()
     $('#component').append($('<option>', { value: 'Select Component', text: 'Select Component' }));
+    $('#version').empty()
+    $('#version').append($('<option>', { value: 'Select Version', text: 'Select Version' }));
 
     $('#product')
     .change(function(event) {
         $('#product-throbber').show();
         $('#component').attr('disabled', true);
+        $('#version').attr('disabled', true);
         $("#product option[value='Select Product']").remove();
         bugzilla_ajax(
             {
-                url: 'rest/bug_modal/components?product=' + encodeURIComponent($('#product').val())
+                url: 'rest/bug_modal/product_info?product=' + encodeURIComponent($('#product').val())
             },
             function(data) {
                 $('#product-throbber').hide();
@@ -35,14 +38,33 @@ $(document).ready(function() {
                 $.each(data.components, function(key, value) {  
                     $('#component').append('<option value=' + value.name + ' desc=' + value.description.split(' ').join('_') + '>' + value.name + '</option>');
                 });
+                $('#version').attr('disabled', false);
+                $('#version').empty();
+                $('#version').append($('<option>', { value: 'Select Version', text: 'Select Version' }));
+                $.each(data.versions, function(key, value) {  
+                    $('#version').append('<option value=' + value.name.split(' ').join('_') + '>' + value.name + '</option>');
+                });
             },
             function() {}
         );
     });
     $('#component')
     .change(function(event) {
-        $("#component option[value='Select Product']").remove();
+        $("#component option[value='Select Component']").remove();
         $('#comp_desc').text($('#component').find(":selected").attr('desc').split('_').join(' '));
     });
+
+    $('#version')
+    .change(function(event) {
+        $("#version option[value='Select Version']").remove();
+    });
+
+    $('.create-btn')
+        .click(function(event) {
+            event.preventDefault();
+            if (document.newbugform.checkValidity && !document.newbugform.checkValidity())
+                return;
+            this.form.submit()
+        });
     
 });
