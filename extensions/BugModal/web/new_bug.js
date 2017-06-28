a07faf6384a03b87d60e97599f49f3629cfaa295
Sebastin Santy <sebastinssanty@gmail.com>
2017-06-29 03:28:00 +0530

    selectize implementation

--- extensions/BugModal/web/new_bug.js
+++ extensions/BugModal/web/new_bug.js
@@ -1,63 +1,89 @@
-$(document).ready(function() {
+var products = []
+var product_info = []
+
+function load_products(query, callback) {
     bugzilla_ajax(
             {
                 url: 'rest/bug_modal/products'
             },
             function(data) {
-                $('#product').empty()
-                $('#product').append($('<option>', { value: 'Select Product', text: 'Select Product' }));
-                // populate select menus
-                $.each(data.products, function(key, value) {
-                    $('#product').append($('<option>', { value: value.name, text: value.name }));
-                });
+                var products = data.products;
+                callback(data.products);
             },
-            function() {}
+            function() {
+                callback();
+            }
         );
-
-    $('#component').empty()
-    $('#component').append($('<option>', { value: 'Select Component', text: 'Select Component' }));
-    $('#version').empty()
-    $('#version').append($('<option>', { value: 'Select Version', text: 'Select Version' }));
-
-    $('#product')
-    .change(function(event) {
-        $('#product-throbber').show();
-        $('#component').attr('disabled', true);
-        $('#version').attr('disabled', true);
-        $("#product option[value='Select Product']").remove();
-        bugzilla_ajax(
-            {
-                url: 'rest/bug_modal/product_info?product=' + encodeURIComponent($('#product').val())
-            },
-            function(data) {
-                $('#product-throbber').hide();
-                $('#component').attr('disabled', false);
-                $('#component').empty();
-                $('#component').append($('<option>', { value: 'Select Component', text: 'Select Component' }));
-                $('#comp_desc').text('Select a component to read its description.');
-                $.each(data.components, function(key, value) {  
-                    $('#component').append('<option value=' + value.name + ' desc=' + value.description.split(' ').join('_') + '>' + value.name + '</option>');
-                });
-                $('#version').attr('disabled', false);
-                $('#version').empty();
-                $('#version').append($('<option>', { value: 'Select Version', text: 'Select Version' }));
-                $.each(data.versions, function(key, value) {  
-                    $('#version').append('<option value=' + value.name.split(' ').join('_') + '>' + value.name + '</option>');
-                });
-            },
-            function() {}
-        );
+}
+$(document).ready(function() {
+    $("#product").selectize({
+        valueField: 'product',
+        labelField: 'product',
+        searchField: 'product',
+        options: [],
+        preload: true,
+        create: false,
+        load: load_products
     });
-    $('#component')
-    .change(function(event) {
-        $("#component option[value='Select Component']").remove();
-        $('#comp_desc').text($('#component').find(":selected").attr('desc').split('_').join(' '));
-    });
+    // bugzilla_ajax(
+    //         {
+    //             url: 'rest/bug_modal/products'
+    //         },
+    //         function(data) {
+    //             $('#product').empty()
+    //             $('#product').append($('<option>', { value: 'Select Product', text: 'Select Product' }));
+    //             // populate select menus
+    //             $.each(data.products, function(key, value) {
+    //                 $('#product').append($('<option>', { value: value.name, text: value.name }));
+    //             });
+    //         },
+    //         function() {}
+    //     );
 
-    $('#version')
-    .change(function(event) {
-        $("#version option[value='Select Version']").remove();
-    });
+    // $('#component').empty()
+    // $('#component').append($('<option>', { value: 'Select Component', text: 'Select Component' }));
+    // $('#version').empty()
+    // $('#version').append($('<option>', { value: 'Select Version', text: 'Select Version' }));
+
+    // $('#product')
+    // .change(function(event) {
+    //     $('#product-throbber').show();
+    //     $('#component').attr('disabled', true);
+    //     $('#version').attr('disabled', true);
+    //     $("#product option[value='Select Product']").remove();
+    //     bugzilla_ajax(
+    //         {
+    //             url: 'rest/bug_modal/product_info?product=' + encodeURIComponent($('#product').val())
+    //         },
+    //         function(data) {
+    //             $('#product-throbber').hide();
+    //             $('#component').attr('disabled', false);
+    //             $('#component').empty();
+    //             $('#component').append($('<option>', { value: 'Select Component', text: 'Select Component' }));
+    //             $('#comp_desc').text('Select a component to read its description.');
+    //             $.each(data.components, function(key, value) {  
+    //                 $('#component').append('<option value=' + value.name + ' desc=' + value.description.split(' ').join('_') + '>' + value.name + '</option>');
+    //             });
+    //             $('#version').attr('disabled', false);
+    //             $('#version').empty();
+    //             $('#version').append($('<option>', { value: 'Select Version', text: 'Select Version' }));
+    //             $.each(data.versions, function(key, value) {  
+    //                 $('#version').append('<option value=' + value.name.split(' ').join('_') + '>' + value.name + '</option>');
+    //             });
+    //         },
+    //         function() {}
+    //     );
+    // });
+    // $('#component')
+    // .change(function(event) {
+    //     $("#component option[value='Select Component']").remove();
+    //     $('#comp_desc').text($('#component').find(":selected").attr('desc').split('_').join(' '));
+    // });
+
+    // $('#version')
+    // .change(function(event) {
+    //     $("#version option[value='Select Version']").remove();
+    // });
 
     $('.create-btn')
         .click(function(event) {
