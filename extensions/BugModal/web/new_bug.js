eee7f181f5b74972d97cd9860b4feddeada4a4cf
Dylan William Hardison <dylan@hardison.net>
2017-06-30 01:55:09 -0400

    more suggested changes

--- extensions/BugModal/web/new_bug.js
+++ extensions/BugModal/web/new_bug.js
@@ -7,7 +7,6 @@ function load_products(query, callback) {
                 url: 'rest/bug_modal/products'
             },
             function(data) {
-                var products = data.products;
                 callback(data.products);
             },
             function() {
@@ -15,75 +14,29 @@ function load_products(query, callback) {
             }
         );
 }
+
 $(document).ready(function() {
-    $("#product").selectize({
-        valueField: 'product',
-        labelField: 'product',
-        searchField: 'product',
+    var product_sel = $("#product").selectize({
+        valueField: 'name',
+        labelField: 'name',
+        searchField: 'name',
         options: [],
         preload: true,
         create: false,
         load: load_products
     });
-    // bugzilla_ajax(
-    //         {
-    //             url: 'rest/bug_modal/products'
-    //         },
-    //         function(data) {
-    //             $('#product').empty()
-    //             $('#product').append($('<option>', { value: 'Select Product', text: 'Select Product' }));
-    //             // populate select menus
-    //             $.each(data.products, function(key, value) {
-    //                 $('#product').append($('<option>', { value: value.name, text: value.name }));
-    //             });
-    //         },
-    //         function() {}
-    //     );
+    var component_sel = $("#component").selectize({
+        valueField: 'name',
+        labelField: 'name',
+        searchField: 'name',
+        options: [],
+    });
 
-    // $('#component').empty()
-    // $('#component').append($('<option>', { value: 'Select Component', text: 'Select Component' }));
-    // $('#version').empty()
-    // $('#version').append($('<option>', { value: 'Select Version', text: 'Select Version' }));
-
-    // $('#product')
-    // .change(function(event) {
-    //     $('#product-throbber').show();
-    //     $('#component').attr('disabled', true);
-    //     $('#version').attr('disabled', true);
-    //     $("#product option[value='Select Product']").remove();
-    //     bugzilla_ajax(
-    //         {
-    //             url: 'rest/bug_modal/product_info?product=' + encodeURIComponent($('#product').val())
-    //         },
-    //         function(data) {
-    //             $('#product-throbber').hide();
-    //             $('#component').attr('disabled', false);
-    //             $('#component').empty();
-    //             $('#component').append($('<option>', { value: 'Select Component', text: 'Select Component' }));
-    //             $('#comp_desc').text('Select a component to read its description.');
-    //             $.each(data.components, function(key, value) {  
-    //                 $('#component').append('<option value=' + value.name + ' desc=' + value.description.split(' ').join('_') + '>' + value.name + '</option>');
-    //             });
-    //             $('#version').attr('disabled', false);
-    //             $('#version').empty();
-    //             $('#version').append($('<option>', { value: 'Select Version', text: 'Select Version' }));
-    //             $.each(data.versions, function(key, value) {  
-    //                 $('#version').append('<option value=' + value.name.split(' ').join('_') + '>' + value.name + '</option>');
-    //             });
-    //         },
-    //         function() {}
-    //     );
-    // });
-    // $('#component')
-    // .change(function(event) {
-    //     $("#component option[value='Select Component']").remove();
-    //     $('#comp_desc').text($('#component').find(":selected").attr('desc').split('_').join(' '));
-    // });
-
-    // $('#version')
-    // .change(function(event) {
-    //     $("#version option[value='Select Version']").remove();
-    // });
+    product_sel.on("change", function () {
+        component_sel.clearOptions();
+        // call component_sel.addOption(data) for each component
+        // call component_sel.refreshOptions() when done
+    });
 
     $('.create-btn')
         .click(function(event) {
