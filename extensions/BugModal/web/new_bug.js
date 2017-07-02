1e122db6afede0446bb4cf2dd77ccaabd1f8e196
Sebastin Santy <sebastinssanty@gmail.com>
2017-07-03 02:25:48 +0530

    Add component description

--- extensions/BugModal/web/new_bug.js
+++ extensions/BugModal/web/new_bug.js
@@ -1,3 +1,5 @@
+var comp_desc = {}
+
 function load_products(query, callback) {
     bugzilla_ajax(
             {
@@ -37,17 +39,26 @@ $(document).ready(function() {
     });
 
     product_sel.on("change", function () {
+        $('#product-throbber').show();
+        $('#component').attr('disabled', true);
         bugzilla_ajax(
                 {
                     url: 'rest/bug_modal/product_info?product=' + encodeURIComponent($('#product').val())
                 },
                 function(data) {
+                    product_info = data;
+                    $('#product-throbber').hide();
+                    $('#component').attr('disabled', false);
+                    $('#comp_desc').text('Select a component to read its description.');
                     var selectize = $("#component")[0].selectize;
                     selectize.clear();
                     selectize.clearOptions();
                     selectize.load(function(callback) {
                         callback(data.components)
                     });
+
+                    for (var i in data.components)
+                        comp_desc[data.components[i]["name"]] = data.components[i]["description"];
 
                     selectize = $("#version")[0].selectize;
                     selectize.clear();
@@ -60,6 +71,11 @@ $(document).ready(function() {
                     alert("Network issues. Please refresh the page and try again");
                 }
             );     
+    });
+
+    component_sel.on("change", function () {
+        var selectize = $("#component")[0].selectize;
+        $('#comp_desc').text(comp_desc[selectize.getValue()]);
     });
 
     $('.create-btn')
