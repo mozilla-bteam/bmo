98a7ff7637a5c6a8c829046f1df3e8be182ac718
Mary <mumoh@mozilla.com>
2017-08-08 10:49:19 -0400

    Updates

--- Bugzilla/Install/Filesystem.pm
+++ Bugzilla/Install/Filesystem.pm
@@ -950,11 +950,10 @@ sub _check_web_server_group {
     return $group_id;
 }
 
-sub robots_txt {
-    my $output = '';
-    Bugzilla->template->process("global/robots.txt.tmpl", {SITEMAP_URL => correct_urlbase().SITEMAP_URL}, \$output) or die Bugzilla->template->error;
-    return $output;
-}
+my %vars;
+Bugzilla::Hook::process("before_robots_txt", { vars => \%vars });
+Bugzilla->template->process("robots.txt.tmpl", \%vars, \$output)
+    or die Bugzilla->template->error;
 
 
 1;
