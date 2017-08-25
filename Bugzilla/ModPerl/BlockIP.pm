b037b24c731a312a238a3a92aa6ccef2a12adeb6
Dylan William Hardison <dylan@hardison.net>
2017-08-25 15:08:27 -0400

    fix some nits

--- Bugzilla/ModPerl/BlockIP.pm
+++ Bugzilla/ModPerl/BlockIP.pm
@@ -5,15 +5,15 @@ use warnings;
 use Apache2::RequestRec ();
 use Apache2::Connection ();
 
-use Apache2::Const -compile => qw(AUTH_REQUIRED OK);
+use Apache2::Const -compile => qw(DONE OK);
 
 sub handler {
     my $r = shift;
     my $ip = $r->headers_in->{'X-Forwarded-For'} // $r->connection->remote_ip;
+    warn "IP: $ip\n";
 
     return $ip eq '96.58.158.18'
-        ? Apache2::Const::DONE
-        : Apache2::Const::OK;
+        ? DONE : OK;
 }
 
 1;
