93388c326b80160a92c7da826a3e818f3d3a46dd
Dylan William Hardison <dylan@hardison.net>
2017-08-28 18:07:40 -0400

    add audit log

--- Bugzilla/Auth/Verify/DB.pm
+++ Bugzilla/Auth/Verify/DB.pm
@@ -64,9 +64,11 @@ sub check_credentials {
     {
         my $pwqc = Bugzilla->passwdqc;
         unless ($pwqc->validate_password($password)) {
+            my $reason = $pwqc->reason;
+            Bugzilla->audit(sprintf "%s logged in with a weak password (reason: %s)", $user->login, $reason);
             $user->set_password_change_required(1);
             $user->set_password_change_reason(
-                "You must change your password for the following reason: " . $pwqc->reason
+                "You must change your password for the following reason: $reason"
             );
             $user->update();
         }
