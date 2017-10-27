2362f427ec49b2093690dfc32a225f62d3829ca6
Dylan William Hardison <dylan@hardison.net>
2017-10-27 11:15:41 -0400

    Bug 1393146 - Automate blocking IPs that bugzilla flags as exceeding rate limits

--- Bugzilla.pm
+++ Bugzilla.pm
@@ -852,9 +852,12 @@ sub check_rate_limit {
             if ($filter && $filter->test($ip)) {
                 $action = 'ignore';
             }
-            my $limit = join("/", @$limit);
-            Bugzilla->audit("[rate_limit] action=$action, ip=$ip, limit=$limit");
-            ThrowUserError("rate_limit") if $action eq 'block';
+            my $limit_str = join("/", @$limit);
+            Bugzilla->audit("[rate_limit] action=$action, ip=$ip, limit=$limit_str");
+            if ($action eq 'block') {
+                Bugzilla::ModPerl::BlockIP->block_ip($ip) if $ENV{MOD_PERL};
+                ThrowUserError("rate_limit");
+            }
         }
     }
 }
