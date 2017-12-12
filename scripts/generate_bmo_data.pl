5620d77b36fd71c1b5ff19f13674a25fde7981aa
Dylan William Hardison <dylan@hardison.net>
2017-12-12 17:42:04 -0500

    fix header and add use 5.10.1

--- scripts/generate_bmo_data.pl
+++ scripts/generate_bmo_data.pl
@@ -3,11 +3,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+use 5.10.1;
 use strict;
 use warnings;
 use lib qw(. lib local/lib/perl5);
-
-
 
 use Bugzilla;
 use Bugzilla::User;
