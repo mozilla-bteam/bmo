50bc479d60659bc5ba25581774ab22b1cb885dbc
Upasana <me@upasana.me>
2018-02-27 19:19:12 +0100

    Added name and id accessors to Review extension

--- extensions/Review/lib/FlagStateActivity.pm
+++ extensions/Review/lib/FlagStateActivity.pm
@@ -35,6 +35,19 @@ use constant DB_COLUMNS => qw(
     status
 );
 
+###############################
+####      Accessors      ######
+###############################
+
+use constant NAME_FIELD => 'name';
+use constant ID_FIELD   => 'id';
+
+use Class::XSAccessor {
+    accessors => {
+        id   => ID_FIELD,
+        name => NAME_FIELD,
+    },
+};
 
 sub _check_param_required {
     my ($param) = @_;
