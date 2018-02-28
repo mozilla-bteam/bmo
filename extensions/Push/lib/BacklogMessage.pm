3680da67034bc8fc494de82be372c5495be26643
Upasana <me@upasana.me>
2018-02-28 17:54:23 +0100

    Use __PACKAGE__ and removed redundant accessor

--- extensions/Push/lib/BacklogMessage.pm
+++ extensions/Push/lib/BacklogMessage.pm
@@ -79,13 +79,9 @@ sub create_from_message {
 # accessors
 #
 
-use constant NAME_FIELD => 'name';
-use constant ID_FIELD   => 'id';
-
 use Class::XSAccessor {
     accessors => {
-        id   => ID_FIELD,
-        name => NAME_FIELD,
+        id   => __PACKAGE__->ID_FIELD,
     },
 };
 
