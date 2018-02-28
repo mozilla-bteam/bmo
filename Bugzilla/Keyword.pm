e487089ba01c1b55e677316c2af92379ac06d536
Upasana <me@upasana.me>
2018-02-28 16:59:17 +0100

    Use __PACKAGE__ constants

--- Bugzilla/Keyword.pm
+++ Bugzilla/Keyword.pm
@@ -47,13 +47,10 @@ use constant UPDATE_COLUMNS => qw(
 ####      Accessors      ######
 ###############################
 
-use constant NAME_FIELD => 'name';
-use constant ID_FIELD   => 'id';
-
 use Class::XSAccessor {
     accessors => {
-        id   => ID_FIELD,
-        name => NAME_FIELD,
+        id   => __PACKAGE__->ID_FIELD,
+        name => __PACKAGE__->NAME_FIELD,
     },
 };
 
