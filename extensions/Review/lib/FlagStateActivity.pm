154a1dcd6e48fff272488fa8b367d0572604527e
Upasana <me@upasana.me>
2018-02-28 17:56:35 +0100

    Use __PACKAGE__ constants

--- extensions/Review/lib/FlagStateActivity.pm
+++ extensions/Review/lib/FlagStateActivity.pm
@@ -39,13 +39,10 @@ use constant DB_COLUMNS => qw(
 ####      Accessors      ######
 ###############################
 
-use constant NAME_FIELD => 'name';
-use constant ID_FIELD   => 'id';
-
 use Class::XSAccessor {
     accessors => {
-        id   => ID_FIELD,
-        name => NAME_FIELD,
+        id   => __PACKAGE__->ID_FIELD,
+        name => __PACKAGE__->NAME_FIELD,
     },
 };
 
