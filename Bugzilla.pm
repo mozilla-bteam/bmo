22b8ca5dd5fa5147cc1995914c20272a66dd1439
Dylan William Hardison <dylan@hardison.net>
2018-03-15 22:05:34 -0400

    add Bugzilla->urlbase method that returns a URI object

--- Bugzilla.pm
+++ Bugzilla.pm
@@ -58,6 +58,7 @@ use File::Spec::Functions;
 use Safe;
 use Sys::Syslog qw(:DEFAULT);
 use JSON::XS qw(decode_json);
+use URI;
 
 use parent qw(Bugzilla::CPAN);
 
@@ -283,6 +284,13 @@ sub input_params {
 
 sub localconfig {
     return $_[0]->process_cache->{localconfig} ||= read_localconfig();
+}
+
+sub urlbase {
+    my ($class) = @_;
+
+    # Since this could be modified, we have to return a new one every time.
+    return URI->new($class->localconfig->{urlbase});
 }
 
 sub params {
