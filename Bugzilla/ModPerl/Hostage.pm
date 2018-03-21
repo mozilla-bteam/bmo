f14acc113fab26fbb24d59633f9f6b53e86a805c
Dylan William Hardison <dylan@hardison.net>
2018-03-21 16:38:29 -0400

    improve the hostage situation

--- Bugzilla/ModPerl/Hostage.pm
+++ Bugzilla/ModPerl/Hostage.pm
@@ -13,41 +13,53 @@ use constant BLOCK_TIMEOUT => 60*60;
 
 sub handler {
     my $r = shift;
+    state $urlbase         = Bugzilla->localconfig->{urlbase};
+    state $urlbase_uri     = URI->new($urlbase);
+    state $urlbase_host    = $urlbase_uri->host;
+    state $urlbase_host_regex = qr/^bug(\d+)\.\Q$urlbase_host\E$/;
+    state $attachment_base = Bugzilla->localconfig->{attachment_base};
+    state $attachment_root = do {
+        if ($attachment_base && $attachment_base =~ m{https?://bug\%bug_id\%\.(.+)$}) {
+            $1;
+        }
+    };
+    state $attachment_regex = do {
+        if ($attachment_base) {
+            my $regex = quotemeta $attachment_base;
+            $regex =~ s/\\\%bugid\\\%/\\d+/g;
+            qr/^$regex$/s;
+        }
+    };
+
     my $hostname  = $r->hostname;
     my $path      = $r->uri;
 
-    return OK if $hostname eq 'bugzilla.mozilla.org';
+    return OK if $hostname eq $urlbase_host;
 
-    if ($hostname eq 'bmoattachments.org') {
+    if ($hostname eq $attachment_root) {
         if ($path eq '/hstsping') {
             return OK;
         }
         else {
-            $r->headers_out->set(Location => 'https://bugzilla.mozilla.org');
+            $r->headers_out->set(Location => $urlbase);
             return REDIRECT;
         }
     }
-    elsif ($hostname =~ m{^bug(\d+)\.bmoattachments.org$} && ! $path =~ m{^/attachment\.cgi}s) {
+    elsif ($hostname =~ $attachment_regex && ! $path =~ m{^/attachment\.cgi}s) {
         my $new_uri = URI->new($r->unparsed_uri);
-        $new_uri->scheme('');
-        $new_uri->host('bugzilla.mozilla.org');
+        $new_uri->scheme($urlbase_uri->scheme);
+        $new_uri->host($urlbase_host);
         $r->headers_out->set(Location => $new_uri);
         return REDIRECT;
     }
-    elsif (my ($id) = $hostname =~ m{^bug(\d+)\.bugzilla\.mozilla.org$}) {
-        my $new_uri = URI->new('https://bugzilla.mozilla.org');
+    elsif (my ($id) = $hostname =~ $urlbase_host_regex) {
+        my $new_uri = $urlbase_uri->clone;
         $new_uri->path("/show_bug.cgi?id=$id");
         $r->headers_out->set(Location => $new_uri);
         return REDIRECT;
     }
 
     return NOT_FOUND;
-}
-
-# FIXME: make this use actual configuration values.
-sub _get_config {
-    return {
-    }
 }
 
 1;
