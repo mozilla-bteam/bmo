02cf1767c0fed69dc46e06cd23a1e9c0c1e59e91
David Lawrence <dkl@mozilla.com>
2018-04-11 16:12:16 -0400

    Make poll seconds environmentally configurable

--- extensions/PhabBugz/lib/Constants.pm
+++ extensions/PhabBugz/lib/Constants.pm
@@ -24,8 +24,10 @@ our @EXPORT = qw(
 use constant PHAB_ATTACHMENT_PATTERN => qr/^phabricator-D(\d+)/;
 use constant PHAB_AUTOMATION_USER    => 'phab-bot@bmo.tld';
 use constant PHAB_CONTENT_TYPE       => 'text/x-phabricator-request';
-use constant PHAB_FEED_POLL_SECONDS  => 5;
-use constant PHAB_USER_POLL_SECONDS  => 60;
-use constant PHAB_GROUP_POLL_SECONDS => 300;
+use constant PHAB_FEED_POLL_SECONDS  => $ENV{PHAB_FEED_POLL} // 5;
+use constant PHAB_USER_POLL_SECONDS  => $ENV{PHAB_USER_POLL} // 60;
+use constant PHAB_GROUP_POLL_SECONDS => $ENV{PHAB_GROUP_POLL} // 300;
+
+@dklawren
 
 1;
