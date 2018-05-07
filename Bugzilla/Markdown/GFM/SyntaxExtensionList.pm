f43d879deb19057f3385bf3c26b6921d6e01be80
Dylan William Hardison <dylan@hardison.net>
2018-05-07 18:23:22 -0400

    Bug 1456877 - Add a wrapper around libcmark_gfm to Bugzilla

---
+++ Bugzilla/Markdown/GFM/SyntaxExtensionList.pm
@@ -0,0 +1,21 @@
+package Bugzilla::Markdown::GFM::SyntaxExtensionList;
+use strict;
+use warnings;
+
+use Data::Printer;
+
+sub SETUP {
+    my ($class, $FFI) = @_;
+
+    $FFI->custom_type(
+        markdown_syntax_extension_list_t => {
+            native_type    => 'opaque',
+            native_to_perl => sub {
+                bless \$_[0], $class if $_[0];
+            },
+            perl_to_native => sub { $_[0] ? ${ $_[0] } : 0 },
+        }
+    );
+}
+
+1;
