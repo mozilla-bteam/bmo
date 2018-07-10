34c7e3f3a73141f2fd1cbe4e2de7a22eeb3774c4
Dylan William Hardison <dylan@hardison.net>
2018-07-10 14:32:06 -0400

    nit from review

--- Bugzilla/Config/General.pm
+++ Bugzilla/Config/General.pm
@@ -33,7 +33,7 @@ use constant get_param_list => (
 
     {
         name    => 'utf8',
-        type    => 't',
+        type    => 's',
         choices => [ '1', 'utf8', 'utf8mb4' ],
         default => 'utf8',
         checker => \&check_utf8
