0366a907e67cc032c3845e53f304a830f2827fff
Kohei Yoshino <kohei.yoshino@gmail.com>
2018-07-17 00:05:54 -0400

    Add buttons to component description pages

--- extensions/ComponentWatching/web/js/overlay.js
+++ extensions/ComponentWatching/web/js/overlay.js
@@ -12,26 +12,27 @@
 var Bugzilla = Bugzilla || {};
 
 /**
- * Implement the basic One-Click Component Watching functionality.
+ * Implement the one-click Component Watching functionality that can be added to any page.
  * @abstract
  */
 Bugzilla.ComponentWatching = class ComponentWatching {
   /**
-   * Initialize a new ComponentWatching instance.
+   * Initialize a new ComponentWatching instance. Since constructors can't be async, use a separate function to move on.
    */
   constructor() {
-    this.api_endpoint = '/rest/component-watching';
-    this.tracking_category = 'Component Watching';
+    this.buttons = document.querySelectorAll('button.component-watching');
+
+    this.init();
   }
 
   /**
    * Send a REST API request, and return the results in a Promise.
-   * @param {Object} [request] Request data. If omitted, the current watch list will be returned.
+   * @param {Object} [request={}] Request data. If omitted, all the current watches will be returned.
+   * @param {String} [path=''] Optional path to be appended to the request URL.
    * @returns {Promise<Object|String>} Response data or error message.
    */
-  async fetch(request = {}) {
-    request.url = this.api_endpoint + (request.path || '');
-    delete request.path;
+  async fetch(request = {}, path = '') {
+    request.url = `/rest/component-watching${path}`;
 
     return new Promise((resolve, reject) => bugzilla_ajax(request, data => resolve(data), error => reject(error)));
   }
@@ -39,7 +40,7 @@ Bugzilla.ComponentWatching = class ComponentWatching {
   /**
    * Start watching the current product or component.
    * @param {String} product Product name.
-   * @param {String} [component] Component name. If omitted, all components in the product will be watched.
+   * @param {String} [component=''] Component name. If omitted, all the components in the product will be watched.
    * @returns {Promise<Object|String>} Response data or error message.
    */
   async watch(product, component = '') {
@@ -48,126 +49,95 @@ Bugzilla.ComponentWatching = class ComponentWatching {
 
   /**
    * Stop watching the current product or component.
-   * @param {Number} id Watching ID.
+   * @param {Number} id ID of the watch to be removed.
    * @returns {Promise<Object|String>} Response data or error message.
    */
   async unwatch(id) {
-    return this.fetch({ type: 'DELETE', path: `/${id}` });
+    return this.fetch({ type: 'DELETE' }, `/${id}`);
   }
 
   /**
    * Log an event with Google Analytics if possible. For privacy reasons, we don't send any specific product or
    * component name.
+   * @param {String} source Event source that will be part of the event category.
    * @param {String} action `watch` or `unwatch`.
    * @param {String} type `product` or `component`.
    * @param {Number} code `0` for a successful change, `1` otherwise.
    * @see https://developers.google.com/analytics/devguides/collection/analyticsjs/events
    */
-  track_event(action, type, code) {
+  track_event(source, action, type, code) {
     if ('ga' in window) {
-      ga('send', 'event', this.tracking_category, action, type, code);
+      ga('send', 'event', `Component Watching: ${source}`, action, type, code);
     }
   }
-};
-
-/**
- * Implement the One-Click Component Watching buttons on the modal-style bug page. One button is for watching all
- * components in the current product, and another one is for watching just the current product.
- */
-Bugzilla.ComponentWatching.BugModalOverlay = class BugModalOverlay extends Bugzilla.ComponentWatching {
-  /**
-   * Initialize a new BugModalOverlay instance.
-   */
-  constructor() {
-    super();
-
-    this.tracking_category = 'BugModal: Component Watching';
-
-    this.$product_select = document.querySelector('#product');
-    this.$product_watch = document.querySelector('#product-watch-btn');
-    this.$component_select = document.querySelector('#component');
-    this.$component_watch = document.querySelector('#component-watch-btn');
-
-    this.product = this.$product_select.value;
-    this.component = this.$component_select.value;
-    this.watching_product = this.watching_component = false;
-    this.watching_id = undefined;
-
-    this.init();
-  }
 
   /**
-   * Show a short floating message on the page. This code is from bug_modal.js, requiring jQuery.
+   * Show a short floating message if the button is on BugModal. This code is from bug_modal.js, requiring jQuery.
    * @param {String} message Message text.
    */
   show_message(message) {
+    if (!document.querySelector('#floating-message')) {
+      return;
+    }
+
     $('#floating-message-text').text(message);
     $('#floating-message').fadeIn(250).delay(2500).fadeOut();
   }
 
   /**
-   * Update the UI of the buttons depending on the current watch status.
+   * Get all the component watching buttons on the current page.
+   * @param {String} [product] Optional product name.
+   * @param {String} [component] Optional component name.
+   * @returns {HTMLButtonElement[]} List of button elements.
    */
-  update_buttons() {
-    this.$product_watch.disabled = false;
-    this.$product_watch.dataset.action = this.watching_product ? 'unwatch' : 'watch';
-    this.$product_watch.textContent = this.watching_product ? 'Unwatch' : 'Watch';
-    this.$product_watch.title = this.watching_product ?
-      `Stop watching all components in the ${this.product} product` :
-      `Start watching all components in the ${this.product} product`;
+  get_buttons(product = undefined, component = undefined) {
+    let buttons = [...this.buttons];
 
-    this.$component_watch.disabled = this.watching_product;
-    this.$component_watch.dataset.action = this.watching_component ? 'unwatch' : 'watch';
-    this.$component_watch.textContent = this.watching_component ? 'Unwatch' : 'Watch';
-    this.$component_watch.title = this.watching_component ?
-      `Stop watching the ${this.component} component` :
-      `Start watching the ${this.component} component`;
-  }
-
-  /**
-   * Retrieve the current watch list, and initialize the status and UI.
-   */
-  async init() {
-    try {
-      const watches = await this.fetch();
-
-      // Check if the current product is being watched
-      let watch = watches.find(watch => this.product === watch.product_name && !watch.component_name);
-
-      if (watch) {
-        this.watching_product = this.watching_component = true;
-        this.watching_id = watch.id;
-      } else {
-        // Check if the current component is being watched
-        watch = watches.find(watch => this.product === watch.product_name && this.component === watch.component_name);
-
-        if (watch) {
-          this.watching_component = true;
-          this.watching_id = watch.id;
-        }
-      }
-
-      this.$product_watch.addEventListener('click', () => this.button_onclick('product'));
-      this.$component_watch.addEventListener('click', () => this.button_onclick('component'));
-
-      this.update_buttons();
-    } catch (ex) {
-      // Remove the buttons as no actions can be done
-      this.$product_watch.remove();
-      this.$component_watch.remove();
+    if (product) {
+      buttons = buttons.filter($button => $button.dataset.product === product);
     }
+
+    if (component) {
+      buttons = buttons.filter($button => $button.dataset.component === component);
+    }
+
+    return buttons;
   }
 
   /**
-   * Called whenever the product or component's Watch/Unwatch button is clicked. Send a request and update the watch
-   * status accordingly.
-   * @param {String} type `product` or `component`.
+   * Update a Watch/Unwatch button for a product or component.
+   * @param {HTMLButtonElement} $button Button element to be updated.
+   * @param {Boolean} disabled Whether the button has to be disabled.
+   * @param {Number} [watchId] Optional watch ID if the product or component is being watched.
    */
-  async button_onclick(type) {
-    const is_product = type === 'product';
-    const $button = is_product ? this.$product_watch : this.$component_watch;
-    const { action } = $button.dataset;
-    const to_watch = action === 'watch';
+  update_button($button, disabled, watchId = undefined) {
+    const { product, component } = $button.dataset;
+
+    if (watchId) {
+      $button.dataset.watchId = watchId;
+      $button.textContent = 'Unwatch';
+      $button.title = component ?
+        `Stop watching the ${component} component` :
+        `Stop watching all components in the ${product} product`;
+    } else {
+      delete $button.dataset.watchId;
+
+      $button.textContent = 'Watch';
+      $button.title = component ?
+        `Start watching the ${component} component` :
+        `Start watching all components in the ${product} product`;
+    }
+
+    $button.disabled = disabled;
+  }
+
+  /**
+   * Called whenever a Watch/Unwatch button is clicked. Send a request to update the user's watch list, and update the
+   * relevant buttons on the page.
+   * @param {HTMLButtonElement} $button Clicked button element.
+   */
+  async button_onclick($button) {
+    const { product, component, watchId, source } = $button.dataset;
     let message = '';
     let code = 0;
 
@@ -175,29 +145,74 @@ Bugzilla.ComponentWatching.BugModalOverlay = class BugModalOverlay extends Bugzi
     $button.disabled = true;
 
     try {
-      if (to_watch) {
-        await this.watch(this.product, is_product ? '' : this.component).then(watch => this.watching_id = watch.id);
+      if (watchId) {
+        await this.unwatch(watchId);
 
-        message = is_product ?
-          `You are now watching all components in the ${this.product} product` :
-          `You are now watching the ${this.component} component`;
+        if (component) {
+          message = `You are no longer watching the ${component} component`;
+
+          this.get_buttons(product, component).forEach($button => this.update_button($button, false));
+        } else {
+          message = `You are no longer watching all components in the ${product} product`;
+
+          this.get_buttons(product).forEach($button => this.update_button($button, false));
+        }
       } else {
-        await this.unwatch(this.watching_id).then(() => this.watching_id = undefined);
+        const watch = await this.watch(product, component);
 
-        message = is_product ?
-          `You are no longer watching all components in the ${this.product} product` :
-          `You are no longer watching the ${this.component} component`;
+        if (component) {
+          message = `You are now watching the ${component} component`;
+
+          this.get_buttons(product, component).forEach($button => this.update_button($button, false, watch.id));
+        } else {
+          message = `You are now watching all components in the ${product} product`;
+
+          this.get_buttons(product).forEach($button => {
+            if ($button.dataset.component) {
+              this.update_button($button, true);
+            } else {
+              this.update_button($button, false, watch.id);
+            }
+          });
+        }
       }
-
-      this.watching_product = to_watch && is_product;
-      this.watching_component = to_watch;
     } catch (ex) {
       message = 'Your watch list could not be updated. Please try again later.';
       code = 1;
     }
 
     this.show_message(message);
-    this.update_buttons();
-    this.track_event(action, type, code);
+    this.track_event(source, watchId ? 'unwatch' : 'watch', component ? 'component' : 'product', code);
+  }
+
+  /**
+   * Retrieve the current watch list, and initialize all the buttons.
+   */
+  async init() {
+    try {
+      const all_watches = await this.fetch();
+
+      this.get_buttons().forEach($button => {
+        const { product, component } = $button.dataset;
+        const watches = all_watches.filter(watch => watch.product_name === product);
+        const product_watch = watches.find(watch => !watch.component);
+
+        if (!component) {
+          // This button is for product watching
+          this.update_button($button, false, product_watch ? product_watch.id : undefined);
+        } else if (product_watch) {
+          // Disabled the button because all the components in the product is being watched
+          this.update_button($button, true);
+        } else {
+          const watch = watches.find(watch => watch.component_name === component);
+
+          this.update_button($button, false, watch ? watch.id : undefined);
+        }
+
+        $button.addEventListener('click', () => this.button_onclick($button));
+      });
+    } catch (ex) {}
   }
 };
+
+window.addEventListener('DOMContentLoaded', () => new Bugzilla.ComponentWatching(), { once: true });
