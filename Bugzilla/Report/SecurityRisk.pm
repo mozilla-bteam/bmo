e4ce26d450879759c4749219d25235618aa83992
Israel Madueme <purelogiq@gmail.com>
2018-09-26 10:43:43 -0400

    Bug 1496057 - Update Security Bugs Report

--- Bugzilla/Report/SecurityRisk.pm
+++ Bugzilla/Report/SecurityRisk.pm
@@ -13,13 +13,21 @@ use MooX::StrictConstructor;
 
 use Bugzilla::Error;
 use Bugzilla::Status qw(is_open_state);
-use Bugzilla::Util qw(datetime_from);
+use Bugzilla::Util qw(datetime_from diff_arrays);
 use Bugzilla;
+
+use Chart::Clicker;
+use Chart::Clicker::Axis::DateTime;
+use Chart::Clicker::Data::Series;
+use Chart::Clicker::Data::DataSet;
+use Chart::Clicker::Renderer::Point;
+
 use DateTime;
 use List::Util qw(any first sum);
+use Mojo::File qw(tempfile);
 use POSIX qw(ceil);
 use Type::Utils;
-use Types::Standard qw(Num Int Bool Str HashRef ArrayRef CodeRef Map Dict Enum);
+use Types::Standard qw(Num Int Bool Str HashRef ArrayRef CodeRef Map Dict Enum Object);
 
 my $DateTime = class_type { class => 'DateTime' };
 
@@ -59,6 +67,8 @@ has 'initial_bugs' => (
             id         => Int,
             product    => Str,
             sec_level  => Str,
+            status     => Str,
+            is_stalled => Bool,
             is_open    => Bool,
             created_at => $DateTime,
         ],
@@ -107,6 +117,34 @@ has 'results' => (
     ],
 );
 
+has 'deltas' => (
+    is  => 'lazy',
+    isa => Dict [
+        by_sec_keyword => HashRef [
+            Dict [
+                added => ArrayRef [Int],
+                closed => ArrayRef [Int],
+            ],
+        ],
+        by_product => HashRef [
+            Dict [
+                added => ArrayRef [Int],
+                closed => ArrayRef [Int],
+            ],
+        ],
+    ],
+);
+
+has 'graphs' => (
+    is => 'lazy',
+    isa => Dict [
+        bugs_by_sec_keyword_count => Object,
+        bugs_by_sec_keyword_age => Object,
+        bugs_by_product_count => Object,
+        bugs_by_product_age => Object,
+    ],
+);
+
 sub _build_initial_bug_ids {
     # TODO: Handle changes in product (e.g. gravyarding) by searching the events table
     # for changes to the 'product' field where one of $self->products is found in
@@ -136,6 +174,7 @@ sub _build_initial_bugs {
     my $bugs      = {};
     my $bugs_list = Bugzilla::Bug->new_from_list( $self->initial_bug_ids );
     for my $bug (@$bugs_list) {
+        my $is_stalled = grep { lc($_->name) eq 'stalled' } @{ $bug->keyword_objects };
         $bugs->{ $bug->id } = {
             id        => $bug->id,
             product   => $bug->product,
@@ -148,7 +187,9 @@ sub _build_initial_bugs {
                 }
                 @{ $bug->keyword_objects }
             )->name,
-            is_open    => $self->check_open_state->( $bug->status->name ),
+            status => $bug->status->name,
+            is_stalled => scalar $is_stalled,
+            is_open    => $self->_is_bug_open($bug->status->name, scalar $is_stalled),
             created_at => datetime_from( $bug->creation_ts ),
         };
     }
@@ -197,7 +238,7 @@ sub _build_results {
 
     # We must generate a report for each week in the target time interval, regardless of
     # whether anything changed. The for loop here ensures that we do so.
-    for ( my $report_date = $self->end_date; $report_date >= $self->start_date; $report_date->subtract( weeks => 1 ) ) {
+    for ( my $report_date = $self->end_date->clone(); $report_date >= $self->start_date; $report_date->subtract( weeks => 1 ) ) {
         # We rewind events while there are still events existing which occured after the start
         # of the report week. The bugs will reflect a snapshot of how they were at the start of the week.
         # $self->events is ordered reverse chronologically, so the end of the array is the earliest event.
@@ -207,12 +248,14 @@ sub _build_results {
             my $event = @{ $self->events }[$e];
             my $bug   = $bugs->{ $event->{bug_id} };
 
+
             # Undo bug status changes
             if ( $event->{field_name} eq 'bug_status' ) {
-                $bug->{is_open} = $self->check_open_state->( $event->{removed} );
+                $bug->{status} = $event->{removed};
+                $bug->{is_open} = $self->_is_bug_open($bug->{status}, $bug->{is_stalled});
             }
 
-            # Undo keyword changes
+            # Undo sec keyword changes
             if ( $event->{field_name} eq 'keywords' ) {
                 my $bug_sec_level = $bug->{sec_level};
                 if ( $event->{added} =~ /\b\Q$bug_sec_level\E\b/ ) {
@@ -224,6 +267,19 @@ sub _build_results {
                     my $removed_sec = first { $event->{removed} =~ /\b\Q$_\E\b/ } @{ $self->sec_keywords };
                     $bug->{sec_level} = $removed_sec if ($removed_sec);
                 }
+            }
+
+            # Undo stalled keyword changes
+            if ( $event->{field_name} eq 'keywords' ) {
+                if ( $event->{added} =~ /\b\stalled\b/ ) {
+                    # If the stalled keyword was added in this event, remove it:
+                    $bug->{is_stalled} = 0;
+                }
+                if ( $event->{removed} =~ /\b\stalled\b/ ) {
+                    # If the stalled keyword was removed in this event, add it:
+                    $bug->{is_stalled} = 1;
+                }
+                $bug->{is_open} = $self->_is_bug_open($bug->{status}, $bug->{is_stalled});
             }
 
             $e++;
@@ -250,6 +306,140 @@ sub _build_results {
     return [reverse @results];
 }
 
+sub _build_graphs {
+    my ($self)  = @_;
+    my $graphs = {};
+    my $data = [
+        {
+            id => 'bugs_by_sec_keyword_count',
+            title => sprintf('Open security bugs by severity (%s to %s)', $self->start_date->ymd, $self->end_date->ymd),
+            range_label => 'Open Bugs Count',
+            datasets => [
+                map {
+                    my $keyword = $_;
+                    {
+                        name => $_,
+                        keys => [ map { $_->{date}->epoch } @{$self->results} ],
+                        values => [ map { scalar @{$_->{bugs_by_sec_keyword}->{$keyword}->{open}} } @{$self->results} ],
+                    }
+                } @{$self->sec_keywords}
+            ],
+            image_file => tempfile(SUFFIX => '.png'),
+        },
+        {
+            id => 'bugs_by_sec_keyword_age',
+            title => sprintf('Median age of open security bugs by severity (%s to %s)', $self->start_date->ymd, $self->end_date->ymd),
+            range_label => 'Median Age (days)',
+            datasets => [
+                map {
+                    my $keyword = $_;
+                    {
+                        name => $_,
+                        keys => [ map { $_->{date}->epoch } @{$self->results} ],
+                        values => [ map { $_->{bugs_by_sec_keyword}->{$keyword}->{median_age_open} } @{$self->results} ],
+                    }
+                } @{$self->sec_keywords}
+            ],
+            image_file => tempfile(SUFFIX => '.png'),
+        },
+        {
+            id => 'bugs_by_product_count',
+            title => sprintf('Open security bugs by product (%s to %s)', $self->start_date->ymd, $self->end_date->ymd),
+            range_label => 'Open Bugs Count',
+            datasets => [
+                map {
+                    my $product = $_;
+                    {
+                        name => $_,
+                        keys => [ map { $_->{date}->epoch } @{$self->results} ],
+                        values => [ map { scalar @{$_->{bugs_by_product}->{$product}->{open}} } @{$self->results} ],
+                    }
+                } @{$self->products}
+            ],
+            image_file => tempfile(SUFFIX => '.png'),
+        },
+        {
+            id => 'bugs_by_product_age',
+            title => sprintf('Median age of open security bugs by product (%s to %s)', $self->start_date->ymd, $self->end_date->ymd),
+            range_label => 'Median Age (days)',
+            datasets => [
+                map {
+                    my $product = $_;
+                    {
+                        name => $_,
+                        keys => [ map { $_->{date}->epoch } @{$self->results} ],
+                        values => [ map { $_->{bugs_by_product}->{$product}->{median_age_open} } @{$self->results} ],
+                    }
+                } @{$self->products}
+            ],
+            image_file => tempfile(SUFFIX => '.png'),
+        },
+    ];
+
+    foreach my $datum (@$data) {
+        my $cc = Chart::Clicker->new;
+        $cc->title->text($datum->{title});
+        $cc->title->font->size(14);
+        $cc->title->padding->bottom(5);
+        $cc->title->padding->top(5);
+        foreach my $dataset (@{$datum->{datasets}}) {
+            my $series = Chart::Clicker::Data::Series->new(
+                name      => $dataset->{name},
+                values    => $dataset->{values},
+                keys      => $dataset->{keys},
+            );
+            my $ds = Chart::Clicker::Data::DataSet->new(series => [ $series ]);
+            $cc->add_to_datasets($ds);
+        }
+        my $ctx = $cc->get_context('default');
+        $ctx->range_axis->label($datum->{range_label});
+        $ctx->domain_axis(
+            Chart::Clicker::Axis::DateTime->new(
+                position => 'bottom',
+                orientation => 'horizontal',
+                format => "%m/%d",
+            )
+        );
+        $cc->write_output($datum->{image_file});
+        $graphs->{$datum->{id}} = $datum->{image_file};
+    }
+
+    return $graphs;
+}
+
+sub _build_deltas {
+    my ($self)  = @_;
+    my $deltas = { by_product => {}, by_sec_keyword => {}};
+    my $data = [
+        {
+            domain => $self->products,
+            results_key => 'bugs_by_product',
+            deltas_key => 'by_product',
+        },
+        {
+            domain => $self->sec_keywords,
+            results_key => 'bugs_by_sec_keyword',
+            deltas_key => 'by_sec_keyword',
+        }
+    ];
+
+    foreach my $datum (@$data) {
+        foreach my $item (@{$datum->{domain}}) {
+            my $current_result = $self->results->[-1]->{$datum->{results_key}}->{$item};
+            my $last_result = $self->results->[-2]->{$datum->{results_key}}->{$item};
+
+            my @all_bugs_this_week = (@{$current_result->{open}}, @{$current_result->{closed}});
+            my @all_bugs_last_week = (@{$last_result->{open}}, @{$last_result->{closed}});
+
+            my $added_delta = (diff_arrays(\@all_bugs_this_week, \@all_bugs_last_week))[0];
+            my $closed_delta = (diff_arrays($current_result->{closed}, $last_result->{closed}))[0];
+
+            $deltas->{$datum->{deltas_key}}->{$item} = { added => $added_delta, closed => $closed_delta };
+        }
+    }
+    return $deltas;
+}
+
 sub _bugs_by_product {
     my ( $self, $report_date, @bugs ) = @_;
     my $result = {};
@@ -259,7 +449,7 @@ sub _bugs_by_product {
     }
     foreach my $bug (@bugs) {
         # We skip over bugs with no sec level which can happen during event rewinding.
-        if ( $bug->{sec_level} ) {
+        if ( defined $bug->{sec_level} ) {
             push @{ $groups->{ $bug->{product} } }, $bug;
         }
     }
@@ -287,7 +477,7 @@ sub _bugs_by_sec_keyword {
     }
     foreach my $bug (@bugs) {
         # We skip over bugs with no sec level which can happen during event rewinding.
-        if ( $bug->{sec_level} ) {
+        if ( defined $bug->{sec_level} ) {
             push @{ $groups->{ $bug->{sec_level} } }, $bug;
         }
     }
@@ -306,9 +496,15 @@ sub _bugs_by_sec_keyword {
     return $result;
 }
 
+sub _is_bug_open {
+    my ( $self, $status, $is_stalled) = @_;
+    return ($self->check_open_state->($status)) && !($is_stalled);
+}
+
 sub _median {
     # From tlm @ https://www.perlmonks.org/?node_id=474564. Jul 14, 2005
     return sum( ( sort { $a <=> $b } @_ )[ int( $#_ / 2 ), ceil( $#_ / 2 ) ] ) / 2;
 }
+
 
 1;
--- scripts/secbugsreport.pl
+++ scripts/secbugsreport.pl
@@ -25,6 +25,7 @@ use Bugzilla::Report::SecurityRisk;
 use DateTime;
 use URI;
 use JSON::MaybeXS;
+use Mojo::File;
 
 BEGIN { Bugzilla->extensions }
 Bugzilla->usage_mode(USAGE_MODE_CMDLINE);
@@ -35,7 +36,7 @@ exit 0 unless defined $ARGV[0] && defined $ARGV[1] && defined $ARGV[2];
 my $html;
 my $template     = Bugzilla->template();
 my $end_date     = DateTime->new( year => $ARGV[0], month => $ARGV[1], day => $ARGV[2] );
-my $start_date   = $end_date->clone()->subtract( months => 6 );
+my $start_date   = $end_date->clone()->subtract( months => 12 );
 my $report_week  = $end_date->ymd('-');
 my $products     = decode_json( Bugzilla->params->{report_secbugs_products} );
 my $sec_keywords = [ 'sec-critical', 'sec-high' ];
@@ -45,12 +46,14 @@ my $report       = Bugzilla::Report::SecurityRisk->new(
     products     => $products,
     sec_keywords => $sec_keywords
 );
+
 my $vars = {
     urlbase         => Bugzilla->localconfig->{urlbase},
     report_week     => $report_week,
     products        => $products,
     sec_keywords    => $sec_keywords,
     results         => $report->results,
+    deltas          => $report->deltas,
     build_bugs_link => \&build_bugs_link,
 };
 
@@ -58,19 +61,39 @@ $template->process( 'reports/email/security-risk.html.tmpl', $vars, \$html )
     or ThrowTemplateError( $template->error() );
 
 # For now, only send HTML email.
+my @parts = (
+    Email::MIME->create(
+        attributes => {
+            content_type => 'text/html',
+            charset      => 'UTF-8',
+            encoding     => 'quoted-printable',
+        },
+        body_str => $html,
+    ),
+    map {
+        Email::MIME->create(
+            header_str => ['Content-ID' => "<$_.png>",],
+            attributes => {
+                filename     => "$_.png",
+                charset      => 'UTF-8',
+                content_type => 'image/png',
+                disposition  => 'inline',
+                name         => "$_.png",
+                encoding     => 'base64',
+            },
+            body => $report->graphs->{$_}->slurp,
+        )
+    } sort { $a cmp $b } keys %{$report->graphs}
+);
+
 my $email = Email::MIME->create(
     header_str => [
         From    => Bugzilla->params->{'mailfrom'},
         To      => Bugzilla->params->{report_secbugs_emails},
         Subject => "Security Bugs Report for $report_week",
-        'X-Bugzilla-Type' => 'admin'
+        'X-Bugzilla-Type' => 'admin',
     ],
-    attributes => {
-        content_type => 'text/html',
-        charset      => 'UTF-8',
-        encoding     => 'quoted-printable',
-    },
-    body_str => $html,
+    parts => [ @parts ],
 );
 
 MessageToMTA($email);
