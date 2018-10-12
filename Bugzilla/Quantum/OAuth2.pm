2eaa9d5d1349a0d1816856620222c7005888238d
David Lawrence <dkl@mozilla.com>
2018-10-11 23:22:38 -0400

    Bug fixes

--- Bugzilla/Quantum/OAuth2.pm
+++ Bugzilla/Quantum/OAuth2.pm
@@ -20,6 +20,8 @@ use Bugzilla::Token;
 
 use DateTime;
 
+use Mojo::Util qw(secure_compare);
+
 use base qw(Exporter);
 our @EXPORT_OK = qw(oauth2);
 
@@ -82,7 +84,7 @@ sub _resource_owner_confirm_scopes {
       'SELECT * FROM oauth2_client WHERE id = ?',
       undef, $client_id);
     my $vars = {
-      client => $client->{id},
+      client => $client,
       scopes => $scopes_ref,
       token  => issue_session_token('oauth_confirm_scopes')
     };
@@ -198,20 +200,20 @@ sub _verify_auth_code {
     undef, $client_id);
   $client_data || return (0, 'unauthorized_client');
 
-  my $auth_code_data
-    = $dbh->selectrow_hashref(
-    'SELECT * FROM oauth2_auth_code WHERE client_id = ? AND auth_code = ?',
-    undef, $client_id, $auth_code);
+  my $auth_code_data = $dbh->selectrow_hashref(
+    'SELECT UNIX_TIMESTAMP(expires) AS expires, verified, redirect_uri, user_id FROM oauth2_auth_code WHERE client_id = ? AND auth_code = ?',
+    undef, $client_id, $auth_code
+  );
 
   if (!$auth_code_data
     or $auth_code_data->{verified}
     or ($uri ne $auth_code_data->{redirect_uri})
     or ($auth_code_data->{expires} <= time)
-    or !_check_password($client_secret, $client_data->{secret}))
+    or !secure_compare($client_secret, $client_data->{secret}))
   {
     INFO('Auth code does not exist') if !$auth_code;
     INFO('Client secret does not match')
-      if !_check_password($client_secret, $client_data->{secret});
+      if !secure_compare($client_secret, $client_data->{secret});
 
     if ($auth_code) {
       INFO('Client secret does not match')
@@ -242,9 +244,9 @@ sub _verify_auth_code {
   # scopes are those that were requested in the authorization request, not
   # those stored in the client (i.e. what the auth request restriced scopes
   # to and not everything the client is capable of)
-  my $scope_descriptions = $dbh->selectrow_array(
+  my $scope_descriptions = $dbh->selectcol_arrayref(
     'SELECT oauth2_scope.description FROM oauth2_scope
-            JOIN oauth2_scope ON oauth2_scope.id = oauth2_auth_code_scope.scope_id
+            JOIN oauth2_auth_code_scope ON oauth2_scope.id = oauth2_auth_code_scope.scope_id
       WHERE oauth2_auth_code_scope.auth_code = ?', undef, $auth_code
   );
 
@@ -253,23 +255,17 @@ sub _verify_auth_code {
   return ($client_id, undef, {%scope}, $auth_code_data->{user_id});
 }
 
-sub _check_password {
-  my ($hashed_password, $password) = @_;
-  return $hashed_password eq $password ? 1 : 0;
-}
-
 sub _store_access_token {
   my (%args) = @_;
   my ($c, $client, $auth_code, $access_token, $refresh_token, $expires_in,
-    $scope, $old_refresh_token)
+    $scopes, $old_refresh_token)
     = @args{
-    qw/ mojo_controller client_id auth_code access_token refresh_token expires_in scope old_refresh_token /
+    qw/ mojo_controller client_id auth_code access_token refresh_token expires_in scopes old_refresh_token /
     };
   my $dbh = Bugzilla->dbh;
   my ($user_id);
 
   if (!defined $auth_code && $old_refresh_token) {
-
     # must have generated an access token via a refresh token so revoke the
     # old access token and refresh token (also copy required data if missing)
     my $prev_refresh_token
@@ -289,7 +285,7 @@ sub _store_access_token {
               JOIN oauth2_access_token_scope ON scope.id = oauth2_access_token_scope.scope_id
         WHERE access_token = ?', undef, $old_refresh_token
     );
-    $scope //= {map { $_ => 1 } @{$scope_descriptions}};
+    $scopes //= map { $_ => 1 } @{ $scope_descriptions };
 
     $user_id = $prev_refresh_token->{user_id};
   }
@@ -301,7 +297,7 @@ sub _store_access_token {
   }
 
   if (ref $client) {
-    $scope   //= $client->{scope};
+    $scopes //= $client->{scope};
     $user_id //= $client->{user_id};
     $client = $client->{client_id};
   }
@@ -315,32 +311,29 @@ sub _store_access_token {
   }
 
   $dbh->do(
-    'INSERT INTO oauth2_access_token VALUES (?, ?, ?, ?, ?)',
-    undef,
-    $access_token,
-    $refresh_token,
-    $client,
-    Bugzilla->user->id,
+    'INSERT INTO oauth2_access_token VALUES (?, ?, ?, ?, ?)', undef,
+    $access_token,                                            $refresh_token,
+    $client,                                                  $user_id,
     DateTime->from_epoch(epoch => time + $expires_in)
   );
 
   $dbh->do('INSERT INTO oauth2_refresh_token VALUES (?, ?, ?, ?)',
-    undef, $refresh_token, $access_token, $client, Bugzilla->user->id);
+    undef, $refresh_token, $access_token, $client, $user_id);
 
-  foreach my $rqd_scope (keys %{$scope}) {
-    my $db_scope
+  foreach my $rqd_scope (keys %{$scopes}) {
+    my $scope_id
       = $dbh->selectrow_array(
       'SELECT id FROM oauth2_scope WHERE description = ?',
       undef, $rqd_scope);
-    if ($db_scope) {
+    if ($scope_id) {
       foreach my $related (qw/ access_token refresh_token /) {
-        my $table = "oauth2_${related}_scopes";
+        my $table = "oauth2_${related}_scope";
         $dbh->do(
           "INSERT INTO $table VALUES (?, ?, ?)",
           undef,
           $related eq 'access_token' ? $access_token : $refresh_token,
-          $db_scope,
-          $scope->{$rqd_scope}
+          $scope_id,
+          $scopes->{$rqd_scope}
         );
       }
     }
@@ -383,7 +376,8 @@ sub _verify_access_token {
   }
   elsif (
     my $access_token_data = $dbh->selectrow_hashref(
-      'SELECT * FROM oauth2_access_token WHERE access_token = ?', undef,
+      'SELECT UNIX_TIMESTAMP(expires) AS expires, client_id, user_id FROM oauth2_access_token WHERE access_token = ?',
+      undef,
       $access_token
     )
     )
@@ -396,14 +390,13 @@ sub _verify_access_token {
     }
 
     foreach my $scope (@{$scopes_ref // []}) {
-
-      my $db_scope_allowed = $dbh->selectrow_array(
+      my $scope_allowed = $dbh->selectrow_array(
         'SELECT allowed FROM oauth2_access_token_scope
                 JOIN oauth2_scope ON oauth2_access_token_scope.scope_id = oauth2_scope.id
           WHERE scope.description = ? AND access_token = ?', undef, $scope,
         $access_token
       );
-      if (!defined $db_scope_allowed || !$db_scope_allowed) {
+      if (!defined $scope_allowed || !$scope_allowed) {
         INFO("Access token doesn't have scope ($scope)");
         return (0, 'invalid_grant');
       }
