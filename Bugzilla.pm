4e7dbfcd0298ef1e63b489c073e4e093ed68dbac
Dylan William Hardison <dylan@hardison.net>
2018-10-15 23:04:52 -0400

    whitespace

--- Bugzilla.pm
+++ Bugzilla.pm
@@ -676,6 +676,7 @@ sub clear_request_cache {
     my (undef, %option) = @_;
     my $request_cache = request_cache();
     my @except        = $option{except} ? @{ $option{except} } : ();
+
     %{ $request_cache } = map { $_ => $request_cache->{$_} } @except;
 }
 
