7654754a52431e63263712b27145cb7b8195e08a
David Lawrence <dkl@mozilla.com>
2018-10-25 18:35:11 -0400

    Bug 1502198 - GitHubAuth cannot be removed from user_info_class in data/params if extension enabled or all logins will no longer work

--- extensions/GitHubAuth/Extension.pm
+++ extensions/GitHubAuth/Extension.pm
@@ -26,12 +26,14 @@ our $VERSION = '0.01';
 BEGIN {
     # Monkey-patch can() on Bugzilla::Auth::Login::CGI so that our own fail_nodata gets called.
     # Our fail_nodata behaves like CGI's, so this shouldn't be a problem for CGI-based logins.
-
     *Bugzilla::Auth::Login::CGI::can = sub {
         my ($stack, $method) = @_;
-
-        return undef if $method eq 'fail_nodata';
-        return $stack->SUPER::can($method);
+        if (Bugzilla->params->{user_info_class} !~ /\bGitHubAuth\b/
+            || $method ne 'fail_nodata')
+        {
+            return $stack->SUPER::can($method);
+        }
+        return undef;
     };
 }
 
