c827c2c6a26c5dffeb673cf1154f6e2ca69ea13a
Dylan William Hardison <dylan@hardison.net>
2018-11-12 13:32:02 -0500

    fix some lint errors

--- js/buglist.js
+++ js/buglist.js
@@ -1,21 +1,21 @@
 function SetCheckboxes(value) {
-  let elements = document.querySelectorAll("input[type='checkbox'][name^='id_'")
+  let elements = document.querySelectorAll("input[type='checkbox'][name^='id_'");
   for (let item of elements) {
     item.checked = value;
   }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   let check_all = document.getElementById("check_all");
   let uncheck_all = document.getElementById("uncheck_all");
   if (check_all) {
-    check_all.addEventListener('click', event => {
+    check_all.addEventListener("click", event => {
       SetCheckboxes(true);
       event.preventDefault();
     });
   }
   if (uncheck_all) {
-    uncheck_all.addEventListener('click', event => {
+    uncheck_all.addEventListener("click", event => {
       SetCheckboxes(false);
       event.preventDefault();
     });
