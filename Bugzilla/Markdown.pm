04a36cf54102e5716def007e7f99778bd663fa0b
Israel Madueme <purelogiq@gmail.com>
2018-12-11 12:35:19 -0500

    perl fmt

--- Bugzilla/Markdown.pm
+++ Bugzilla/Markdown.pm
@@ -27,13 +27,11 @@ sub _build_markdown_parser {
   if (Bugzilla->has_feature('alien_cmark')) {
     require Bugzilla::Markdown::GFM;
     require Bugzilla::Markdown::GFM::Parser;
-    return Bugzilla::Markdown::GFM::Parser->new(
-      {
-        hardbreaks => 1,
-        validate_utf8 => 1,
-        extensions => [qw( autolink tagfilter table strikethrough )],
-      }
-    );
+    return Bugzilla::Markdown::GFM::Parser->new({
+      hardbreaks    => 1,
+      validate_utf8 => 1,
+      extensions    => [qw( autolink tagfilter table strikethrough )],
+    });
   }
   else {
     return undef;
@@ -44,18 +42,18 @@ sub render_html {
   my ($self, $markdown, $bug, $comment, $user) = @_;
   my $parser             = $self->markdown_parser;
   my $bugzilla_shorthand = $self->bugzilla_shorthand;
-  my @valid_text_tags = ('p', 'li', 'td');
+  my @valid_text_tags    = ('p', 'li', 'td');
 
   if ($parser) {
     my $html = decode('UTF-8', $parser->render_html($markdown));
-    my $dom  = Mojo::DOM->new($html);
+    my $dom = Mojo::DOM->new($html);
     $dom->find(join ', ', @valid_text_tags)->map(sub {
       my $node = shift;
-       $node->descendant_nodes->map(sub {
+      $node->descendant_nodes->map(sub {
         my $child = shift;
-        if ($child->type eq 'text'
-            && $child->children->size == 0
-            && any { $child->parent->tag eq $_ } @valid_text_tags)
+        if ( $child->type eq 'text'
+          && $child->children->size == 0
+          && any { $child->parent->tag eq $_ } @valid_text_tags)
         {
           my $text = $child->content;
           $child->replace(Mojo::DOM->new($bugzilla_shorthand->($text)));
@@ -70,7 +68,6 @@ sub render_html {
     return escape_html($markdown);
   }
 }
-
 
 
 1;
