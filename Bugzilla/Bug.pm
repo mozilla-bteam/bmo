280e55493aa8b427e7cfbd4fb04e2cb917ec2d95
Kohei Yoshino <kohei.yoshino@gmail.com>
2019-03-21 18:15:34 -0400

    Bug 1514000 - Suppress duplicated changes in bug history made at the same time mainly due to mid-air collisions

--- Bugzilla/Bug.pm
+++ Bugzilla/Bug.pm
@@ -4461,8 +4461,10 @@ sub GetBugActivity {
     $suppwhere = "AND COALESCE(attachments.isprivate, 0) = 0";
   }
 
+  # Use DISTINCT and value comparison to surpress duplicated changes wierdly
+  # made at the same time by the same user
   my $query
-    = "SELECT fielddefs.name, bugs_activity.attach_id, "
+    = "SELECT DISTINCT fielddefs.name, bugs_activity.attach_id, "
     . $dbh->sql_date_format('bugs_activity.bug_when', '%Y.%m.%d %H:%i:%s')
     . " AS bug_when, bugs_activity.removed, bugs_activity.added, profiles.login_name,
                bugs_activity.comment_id
@@ -4473,6 +4475,7 @@ sub GetBugActivity {
     INNER JOIN profiles
             ON profiles.userid = bugs_activity.who
          WHERE bugs_activity.bug_id = ?
+           AND bugs_activity.removed != bugs_activity.added
                $datepart
                $attachpart
                $suppwhere ";
@@ -4526,6 +4529,17 @@ sub GetBugActivity {
       = @$entry;
     my %change;
     my $activity_visible = 1;
+    my $last_change = @$changes[-1] || {};
+
+    # Surpress any mid-air collision
+    if ( $when eq $operation->{'when'}
+      && $fieldname eq $last_change->{'fieldname'}
+      && $removed eq $last_change->{'removed'}
+      && $added eq $last_change->{'added'}
+      && $attachid eq $last_change->{'attachid'})
+    {
+      next;
+    }
 
     # check if the user should see this field's activity
     if ( $fieldname eq 'remaining_time'
