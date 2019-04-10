9493dcc2cf6106920c9f6c061ebccd8af43ce84e
Kohei Yoshino <kohei.yoshino@gmail.com>
2019-04-10 10:19:56 -0400

    Bug 1542554 - Add bug type icons to dependency trees

---
+++ js/dependency-tree.js
@@ -0,0 +1,74 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0. */
+
+/**
+ * Reference or define the Bugzilla app namespace.
+ * @namespace
+ */
+var Bugzilla = Bugzilla || {}; // eslint-disable-line no-var
+
+/**
+ * Activate a Dependency Tree so the user can expand/collapse trees and highlight duplicates.
+ */
+Bugzilla.DependencyTree = class DependencyTree {
+  /**
+   * Initialize a new DependencyTree instance.
+   * @param {HTMLUListElement} $tree The topmost element of a tree.
+   */
+  constructor($tree) {
+    $tree.querySelectorAll('.expander').forEach($button => {
+      $button.addEventListener('click', event => this.toggle_treeitem(event));
+    });
+
+    $tree.querySelectorAll('.duplicate-highlighter').forEach($button => {
+      $button.addEventListener('click', event => this.highlight_duplicates(event));
+    });
+
+    $tree.querySelectorAll('.summary.duplicated .bug-link').forEach($link => {
+      $link.addEventListener('mouseenter', event => this.highlight_duplicates(event));
+      $link.addEventListener('mouseleave', event => this.highlight_duplicates(event));
+    });
+  }
+
+  /**
+   * Expand or collapse one or more tree items.
+   * @param {MouseEvent} event `click` event.
+   */
+  toggle_treeitem(event) {
+    const { target, altKey, ctrlKey, metaKey, shiftKey } = event;
+    const $item = target.closest('[role="treeitem"]');
+    const expanded = $item.matches('[aria-expanded="false"]');
+    const accelKey = navigator.platform === 'MacIntel' ? metaKey && !ctrlKey : ctrlKey;
+
+    $item.setAttribute('aria-expanded', expanded);
+
+    // Do the same for the subtrees if the Ctrl/Command key is pressed
+    if (accelKey && !altKey && !shiftKey) {
+      $item.querySelectorAll('[role="treeitem"]').forEach($child => {
+        $child.setAttribute('aria-expanded', expanded);
+      });
+    }
+  }
+
+  /**
+   * Highlight one or more duplicated tree items.
+   * @param {MouseEvent} event `click`, `mouseenter` or `mouseleave` event.
+   */
+  highlight_duplicates(event) {
+    const id = Number(event.target.closest('[role="treeitem"]').dataset.id);
+
+    document.querySelectorAll(`[role="treeitem"][data-id="${id}"] > .summary`).forEach($summary => {
+      $summary.classList.toggle('highlight');
+    });
+  }
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('[role="tree"]').forEach($tree => {
+    new Bugzilla.DependencyTree($tree);
+  });
+}, { once: true });
