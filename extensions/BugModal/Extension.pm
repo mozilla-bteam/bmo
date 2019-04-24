ed64163bd2775db0c193953de141343ffaabdf08
Kohei Yoshino <kohei.yoshino@gmail.com>
2019-04-24 13:32:38 -0400

    Remove format=multiple&id=X support

--- extensions/BugModal/Extension.pm
+++ extensions/BugModal/Extension.pm
@@ -49,11 +49,16 @@ sub _alternative_show_bug_format {
     return '' if $ctype ne 'html';
   }
   if (my $format = $cgi->param('format')) {
-    # Drop `format=default` and 301 Moved Permanently redirect to the modal UI
-    if ($format eq '__default__' || $format eq 'default') {
+    my @ids = $cgi->param('id');
+    # Drop `format=default` as well as `format=multiple`, if a single bug ID is
+    # provided, by redirecting to the modal UI (301 Moved Permanently)
+    if ($format eq '__default__' || $format eq 'default'
+      || ($format eq 'multiple' && scalar(@ids) == 1))
+    {
       $cgi->base_redirect('show_bug.cgi?id=' . $cgi->param('id'), 1);
     }
-    # Otherwise, printable `format=multiple` is still supported
+    # Otherwise, printable `format=multiple` is still available from bug lists
+    # as the Long Format option
     return $format;
   }
   return $user->setting('ui_experiments') eq 'on' ? 'modal' : '';
