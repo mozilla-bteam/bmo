83b60bbf55863df6f59938e647fa55630fdfa0bd
Kohei Yoshino <kohei.yoshino@gmail.com>
2019-05-22 21:05:21 -0400

    Add Light & Dark mode to user pref

--- Bugzilla/User/Setting/Skin.pm
+++ Bugzilla/User/Setting/Skin.pm
@@ -18,7 +18,7 @@ use Bugzilla::Constants;
 use File::Spec::Functions;
 use File::Basename;
 
-use constant BUILTIN_SKIN_NAMES => ['standard'];
+use constant BUILTIN_SKIN_NAMES => ['standard', 'standard-light', 'standard-dark'];
 
 sub legal_values {
   my ($self) = @_;
