1f29cf0636b79f789014cbe134b34bfda8dd3e18
Dylan William Hardison <dylan@hardison.net>
2019-06-07 17:03:24 -0400

    Bug 1557779 - OAuth flow broken when user is not already logged in *and* uses duo auth

--- Bugzilla.pm
+++ Bugzilla.pm
@@ -392,10 +392,14 @@ sub login {
   }
 
   # If Mojo native app is requesting login, we need to possibly redirect
-  my $C = $Bugzilla::App::CGI::C;
-  if ($C->session->{override_login_target}) {
-    my $mojo_url = Mojo::URL->new($C->session->{override_login_target});
-    $mojo_url->query($C->session->{cgi_params});
+  my $C       = $Bugzilla::App::CGI::C;
+  my $session = $C->session;
+
+  if (!$on_token_page && $session->{override_login_target}) {
+    my $override_login_target = delete $session->{override_login_target};
+    my $cgi_params            = delete $session->{cgi_params};
+    my $mojo_url              = Mojo::URL->new($override_login_target);
+    $mojo_url->query($cgi_params);
     $C->redirect_to($mojo_url);
   }
 
