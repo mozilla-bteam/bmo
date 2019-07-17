bugzilla/bugzilla

commit 4150fed3609ae02a8663a513243896f77b02181d

Bug 1556320 - Mismatch between changed summary and current summary

Bugzilla/Bug.pm
@@ -4535,7 +4535,6 @@ sub GetBugActivity {
     INNER JOIN profiles
             ON profiles.userid = bugs_activity.who
          WHERE bugs_activity.bug_id = ?
-           AND bugs_activity.removed != bugs_activity.added
                $datepart
                $attachpart
                $suppwhere ";
@@ -4591,12 +4590,13 @@ sub GetBugActivity {
     my $activity_visible = 1;
     my $last_change = @$changes[-1] || {};
 
-    # Suppress any mid-air collision
-    if ( $when eq $operation->{'when'}
+    # Suppress any mid-air collision or duplicated change
+    if (( $when eq $operation->{'when'}
       && $fieldname eq $last_change->{'fieldname'}
       && $removed eq $last_change->{'removed'}
       && $added eq $last_change->{'added'}
       && $attachid eq $last_change->{'attachid'})
+      || $removed eq $added )
     {
       next;
     }

mozilla-bteam/bmo

commit b85568e0e9e5ea653dfcbe5b75742c597797fba2

Update comment (fallout from #1305)

Bugzilla/Bug.pm
@@ -4539,8 +4539,8 @@ sub GetBugActivity {
     = $dbh->sql_date_format('bugs_activity.bug_when', '%Y-%m-%d %H:%i:%s');
 
   # Use an outer table so alias column `bug_when` will work within the `where`
-  # clause. Also use `DISTINCT` and a value comparison to suppress duplicated
-  # changes weirdly made at the same time by the same user.
+  # clause. Also use `DISTINCT` to suppress duplicated changes weirdly made at
+  # the same time by the same user.
   my $query = "
     SELECT
       field_name, activity_id, attach_id, bug_when, removed, added, who, comment_id