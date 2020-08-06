021a696c0fc8b93de7a1fec6e0c7e8b0ea10f670
David Lawrence <dkl@mozilla.com>
2020-08-06 01:04:02 -0400

    Bug 1657542 - During recent bmo deployment, emails were delivered to a file instead of SES which caused interruption of email service

---
+++ scripts/import_email.pl
@@ -0,0 +1,41 @@
+#!/bin/env perl
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This Source Code Form is "Incompatible With Secondary Licenses", as
+# defined by the Mozilla Public License, v. 2.0.
+#
+# Import email messages from a mbox file and place in jobqueue
+
+use 5.10.1;
+use strict;
+use warnings;
+use lib qw(/app /app/local/lib/perl5);
+
+use Bugzilla;
+
+my $filename = shift;
+die "Need mbox filename\n" if !$filename;
+
+print "Processing $filename\n";
+
+open(FILE, '<', $filename) || die "Could not open mbox file: $!\n";
+binmode FILE, ':utf8';
+
+my ($msg, $count);
+while (my $line = <FILE>) {
+  if ($line =~ /^From - /) {
+    if ($msg) {
+      Bugzilla->job_queue->insert('send_mail', {msg => $msg});
+      $count++;
+    }
+    $msg = undef;
+    next;
+  }
+  $msg .= $line;
+}
+
+close(FILE) || die "Could not close mbox file: $!\n";
+
+print "Imported $count emails\n";
