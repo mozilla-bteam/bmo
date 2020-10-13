09efbdf83b5a00c492c3e43d04873671ac2a1517
David Lawrence <dkl@mozilla.com>
2020-10-13 11:25:54 -0400

    The start of the Auth0 extension

---
+++ extensions/Auth0/Extension.pm
@@ -0,0 +1,20 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This Source Code Form is "Incompatible With Secondary Licenses", as
+# defined by the Mozilla Public License, v. 2.0.
+
+package Bugzilla::Extension::Auth0;
+
+use 5.10.1;
+use strict;
+use warnings;
+
+use base qw(Bugzilla::Extension);
+
+use Bugzilla::Constants;
+
+our $VERSION = '1';
+
+__PACKAGE__->NAME;
