c18b830cd417bf810f9c0d64c4adcaa6749bca49
Tooru Fujisawa <arai_a@mac.com>
2022-07-31 02:31:00 +0900

    Bug 1782341 - Part 12: Add graph to phabricator revisions.

--- extensions/PhabBugz/web/js/phabricator.js
+++ extensions/PhabBugz/web/js/phabricator.js
@@ -110,7 +110,25 @@ function splitBranches(revisions, revMap, branchCount) {
   }
 }
 
+function findFreeIndex(edges) {
+  let index = 0;
+
+  while (true) {
+    if (!edges.has(index)) {
+      return index;
+    }
+    index++;
+  }
+}
+
+const svgns = "http://www.w3.org/2000/svg";
+
+const EdgeMargin = 8;
+
 const Phabricator = {
+  // A map from revision ID to graph's svg element.
+  svgs: new Map(),
+
   // A map from revision ID to table row.
   trs: new Map(),
 
@@ -166,6 +184,255 @@ const Phabricator = {
     this.revisions = revisions.filter(rev => rev != pseudoRoot);
   },
 
+  // Calculate a graph edges.
+  //
+  // The graph uses X-axis as index.
+  //
+  //    0 1 2
+  //
+  //    o
+  //    |
+  //    |\
+  //    | |
+  //    | o
+  //    | |
+  //    | | o
+  //    | | |
+  //    | |/
+  //    | o
+  //    | |\
+  //    | | |
+  //    | | o
+  //    | | |
+  //    | o |
+  //    | |/
+  //    o |
+  //    | |
+  //    |/
+  //    |
+  //    o
+  //
+  // Each revision has the following information:
+  //
+  //   * The node's index
+  //   * List of parent node's index
+  //   * Whether the node has children
+  //   * List of other edge, represented by from/to indices
+  //
+  // That correspnds to the following part in the above graph:
+  //
+  //    | | |
+  //    | o |
+  //    | |/
+  //
+  calculateGraph() {
+    if (!this.revisions.length) {
+      return;
+    }
+
+    let revisions = this.revisions;
+    if (!this.showAbandoned) {
+      revisions = revisions.filter(rev => rev.status !== "abandoned");
+    }
+
+    const revMap = {};
+    for (const rev of revisions) {
+      revMap[rev.id] = rev;
+    }
+
+    // A map from edge's index to a set of remaining children.
+    const edges = new Map();
+
+    let maxEdgeCount = 1;
+    for (const rev of revisions) {
+      const visibleChildren = rev.children.filter(child => child in revMap);
+
+      const graph = {
+        // This node's index.
+        index: null,
+
+        // List of parent node's index.
+        parentIndices: [],
+
+        // True if there are visible children.
+        hasChildren: visibleChildren.length > 0,
+
+        // Othe edges in the same row.
+        otherEdges: [],
+      };
+      rev.graph = graph;
+
+      // Find all parents, and clear the parent's edge if this node is the last
+      // remaining child.
+      for (const [index, edge] of edges) {
+        if (edge.remainingChildren.has(rev.id)) {
+          edge.remainingChildren.delete(rev.id);
+          if (edge.remainingChildren.size == 0) {
+            edges.delete(index);
+          }
+
+          graph.parentIndices.push(index);
+        }
+      }
+
+      // After cleaning up parent edges, the remaining edges are drawn
+      // concurrently.
+      for (const [index, edge] of edges) {
+        graph.otherEdges.push({
+          from: index,
+          to: index,
+        });
+      }
+
+      // Find this node's index.
+      if (graph.parentIndices.length > 0) {
+        for (const parentIndex of graph.parentIndices) {
+          if (!edges.has(parentIndex)) {
+            // If there's any parent where this node is the last remaining
+            // child, use that parent's index.
+            //
+            //   o <- this node
+            //   |
+            //   o <- parent
+            //   |
+            //
+            graph.index = parentIndex;
+            break;
+          }
+        }
+
+        if (graph.index === null) {
+          // If all parents still have remaining children, put this node
+          // at the first parent's index, and move the remaining children of
+          // the first child into a new index.
+          //
+          // | <--- edge for remaining chidlren
+          // |
+          // | o <- this node
+          //  \|
+          //   |
+          //   o <- parent
+          //   |
+          //
+          const parentIndex = graph.parentIndices[0];
+          graph.index = parentIndex;
+          const newParentIndex = findFreeIndex(edges);
+          const parentEdge = edges.get(parentIndex);
+          edges.delete(parentIndex);
+          edges.set(newParentIndex, parentEdge);
+
+          // The edge for the remaining children.
+          graph.otherEdges.push({
+            from: parentIndex,
+            to: newParentIndex,
+          });
+        }
+      } else {
+        // This node has no parent. Put it into a new index.
+        graph.index = findFreeIndex(edges);
+      }
+
+      // Put a new edge to this node's children.
+      if (graph.hasChildren) {
+        edges.set(graph.index, {
+          remainingChildren: new Set(visibleChildren),
+        });
+      }
+
+      maxEdgeCount = Math.max(maxEdgeCount, edges.size);
+    }
+
+    this.maxEdgeCount = maxEdgeCount;
+  },
+
+  svgWidth() {
+    return (this.maxEdgeCount + 1) * EdgeMargin;
+  },
+
+  toNodeX(index) {
+    return EdgeMargin * (1 + index);
+  },
+
+  // Draw graph in the pre-populated svg elements for each revision's row.
+  drawGraph() {
+    const color = "#cc0099";
+
+    for (const rev of this.revisions) {
+      if (!this.showAbandoned) {
+        if (rev.status === "abandoned") {
+          continue;
+        }
+      }
+
+      const svg = this.svgs.get(rev.id);
+
+      // Cleanup the previous result before switching showAbandoned checkbox.
+      svg.setAttribute("width", this.svgWidth());
+      svg.replaceChildren();
+
+      // Fit to the table row.
+      // The height of the row can differ for each, due to:
+      //   * The number of reviewers can be different for each
+      //   * Long title can be wrapped into multiple lines
+      const h = svg.parentNode.offsetHeight;
+      svg.setAttribute("height", h);
+
+      const graph = rev.graph;
+
+      const x = this.toNodeX(graph.index);
+      const y = h / 2;
+
+      // An edge to children.
+      if (graph.hasChildren) {
+        const path = document.createElementNS(svgns, "path");
+        svg.append(path);
+        path.setAttribute("d", `M ${x} ${y} L ${x} 0`);
+        path.setAttribute("fill", "none");
+        path.setAttribute("stroke", color);
+        path.setAttribute("stroke-width", "1");
+      }
+
+      // An edge from parents.
+      for (const index of graph.parentIndices) {
+        const px = this.toNodeX(index);
+
+        const path = document.createElementNS(svgns, "path");
+        svg.append(path);
+        path.setAttribute("d",
+                          `M ${x} ${y} ` +
+                          `C ${x} ${h},  ${px} ${y},  ${px} ${h}`);
+        path.setAttribute("fill", "none");
+        path.setAttribute("stroke", color);
+        path.setAttribute("stroke-width", "1");
+      }
+
+      // Other edges in the same row.
+      for (const {from, to} of graph.otherEdges) {
+        const fromX = this.toNodeX(from);
+        const toX = this.toNodeX(to);
+
+        const path = document.createElementNS(svgns, "path");
+        svg.append(path);
+        path.setAttribute("d",
+                          `M ${toX} 0 ` +
+                          `L ${toX} ${y} ` +
+                          `C ${toX} ${h}, ${fromX} ${y}, ${fromX} ${h}`);
+        path.setAttribute("fill", "none");
+        path.setAttribute("stroke", color);
+        path.setAttribute("stroke-width", "1");
+      }
+
+      // This node.
+      const circle = document.createElementNS(svgns, "circle");
+      svg.append(circle);
+      circle.setAttribute("fill", color);
+      circle.setAttribute("stroke", color);
+      circle.setAttribute("cx", x);
+      circle.setAttribute("cy", y);
+      circle.setAttribute("r", "3");
+    }
+  },
+
   createTable() {
     const phabUrl = document.querySelector(".phabricator-revisions").getAttribute("data-phabricator-base-uri");
 
@@ -176,6 +443,21 @@ const Phabricator = {
     for (const rev of this.revisions.slice().reverse()) {
       const trRevision = document.createElement("tr");
       this.trs.set(rev.id, trRevision);
+
+      // Graph
+
+      const tdGraph = document.createElement("td");
+      tdGraph.style.paddingTop = "0";
+      tdGraph.style.paddingBottom = "0";
+
+      const svg = document.createElementNS(svgns, "svg");
+      svg.setAttribute("xmlns", svgns);
+      svg.setAttribute("version", "1.1");
+      svg.setAttribute("width", this.svgWidth());
+      svg.setAttribute("height", 10);
+      svg.style.display = "block";
+      this.svgs.set(rev.id,  svg);
+      tdGraph.append(svg);
 
       // Revision ID
 
@@ -246,6 +528,7 @@ const Phabricator = {
       }
 
       trRevision.append(
+        tdGraph,
         tdId,
         tdRevisionStatus,
         tdReviewers,
@@ -290,7 +573,9 @@ const Phabricator = {
 
       if (revisions.length) {
         this.setRevisions(revisions);
+        this.calculateGraph();
         this.createTable();
+        this.drawGraph();
       } else {
         displayLoadError("none returned from server");
       }
@@ -303,6 +588,8 @@ const Phabricator = {
     showAbandonedCheckbox.addEventListener("click", event => {
       this.showAbandoned = showAbandonedCheckbox.checked;
       this.updateVisibility();
+      this.calculateGraph();
+      this.drawGraph();
     });
   },
 };
