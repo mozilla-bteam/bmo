7624b287f55d7b60e7a6ea26e0f95ef91f156b83
arai-a <arai_a@mac.com>
2022-08-03 04:35:17 +0900

    Bug 1782341 - Show graph in phabricator revisions

--- extensions/PhabBugz/web/js/phabricator.js
+++ extensions/PhabBugz/web/js/phabricator.js
@@ -6,7 +6,7 @@
  * defined by the Mozilla Public License, v. 2.0.
  */
 
-var Phabricator = {};
+/* global Bugzilla, BUGZILLA */
 
 // Add pseudo root revision that has all root revisions as child.
 function addPseudoRoot(revisions, revMap) {
@@ -112,159 +112,493 @@ function splitBranches(revisions, revMap, branchCount) {
   }
 }
 
-// Sort revisions based on the graph.
-function sortRevisions(revs) {
-  if (!revs.length) {
-    return revs;
-  }
+function findFreeIndex(edges) {
+  let index = 0;
 
-  const revisions = revs.slice();
-
-  const revMap = {};
-  for (const rev of revisions) {
-    // If the data is old, do nothing.
-    if (!rev.children) {
-      return revs;
+  while (true) {
+    if (!edges.has(index)) {
+      return index;
     }
-
-    revMap[rev.id] = rev;
+    index++;
   }
-
-  const pseudoRoot = addPseudoRoot(revisions, revMap);
-
-  // Setup extra fields.
-  let branchCount = 0;
-  for (const rev of revisions) {
-    rev.rank = 1;
-    rev.hasBranch = rev.children.length > 1;
-    rev.isBranchHandled = false;
-
-    if (rev.hasBranch) {
-      branchCount++;
-    }
-  }
-
-  // Make the revisions partially ordered.
-  fixRank(pseudoRoot, revMap);
-  sortByRank(revisions);
-
-  if (branchCount < 8) {
-    // Perform only if the stack is simple enough.
-    splitBranches(revisions, revMap, branchCount);
-  }
-
-  return revisions.filter(rev => rev != pseudoRoot);
 }
 
-Phabricator.getBugRevisions = async () => {
-    var phabUrl = $('.phabricator-revisions').data('phabricator-base-uri');
-    var tr      = $('<tr/>');
-    var td      = $('<td/>');
-    var link    = $('<a/>');
-    var span    = $('<span/>');
-    var table   = $('<table/>');
+const svgns = "http://www.w3.org/2000/svg";
 
-    function revisionRow(revision) {
-        var trRevision     = tr.clone();
-        var tdId           = td.clone();
-        var tdTitle        = td.clone();
-        var tdRevisionStatus       = td.clone();
-        var tdReviewers    = td.clone();
-        var tableReviews   = table.clone();
+const EdgeMargin = 8;
 
-        var spanRevisionStatus     = span.clone();
-        var spanRevisionStatusIcon = span.clone();
-        var spanRevisionStatusText = span.clone();
+const Phabricator = {
+  // A map from revision ID to graph's svg element.
+  svgs: new Map(),
 
-        var revLink = link.clone();
-        revLink.attr('href', phabUrl + revision.id);
-        revLink.text(revision.id);
-        tdId.append(revLink);
+  // A map from revision ID to table row.
+  trs: new Map(),
 
-        tdTitle.text(revision.title);
-        tdTitle.addClass('phabricator-title');
+  // True if abandoned revisions should be shown.
+  showAbandoned: false,
 
-        spanRevisionStatusIcon.addClass('revision-status-icon-' + revision.status);
-        spanRevisionStatus.append(spanRevisionStatusIcon);
-        spanRevisionStatusText.text(revision.long_status);
-        spanRevisionStatus.append(spanRevisionStatusText);
-        spanRevisionStatus.addClass('revision-status-box-' + revision.status);
-        tdRevisionStatus.append(spanRevisionStatus);
+  // A list of revisions, sorted in the stack order, root to leaf.
+  revisions: null,
 
-        var reviews = revision.reviews.slice().sort((a, b) => {
-          return a.user < b.user ? -1 : 1;
-        });
+  // Set revisions and sort them in the stack order.
+  setRevisions(revisions) {
+    this.revisions = revisions.slice();
 
-        var i = 0, l = reviews.length;
-        for (; i < l; i++) {
-            var trReview             = tr.clone();
-            var tdReviewStatus       = td.clone();
-            var tdReviewer           = td.clone();
-            var spanReviewStatusIcon = span.clone();
-            trReview.prop('title', reviews[i].long_status);
-            spanReviewStatusIcon.addClass('review-status-icon-' + reviews[i].status);
-            tdReviewStatus.append(spanReviewStatusIcon);
-            tdReviewer.text(reviews[i].user);
-            tdReviewer.addClass('review-reviewer');
-            trReview.append(tdReviewStatus, tdReviewer);
-            tableReviews.append(trReview);
-        }
-        tableReviews.addClass('phabricator-reviewers');
-        tdReviewers.append(tableReviews);
-
-        trRevision.attr('data-status', revision.status);
-        if (revision.status === 'abandoned') {
-            trRevision.addClass('bz_default_hidden');
-            $('tbody.phabricator-show-abandoned').removeClass('bz_default_hidden');
-        }
-
-        trRevision.append(
-            tdId,
-            tdRevisionStatus,
-            tdReviewers,
-            tdTitle
-        );
-
-        return trRevision;
+    if (!this.revisions.length) {
+      return;
     }
 
-    var tbody = $('tbody.phabricator-revision');
+    revisions = this.revisions;
+
+    const revMap = {};
+    for (const rev of revisions) {
+      // If the data is old, do nothing.
+      if (!rev.children) {
+        return;
+      }
+
+      revMap[rev.id] = rev;
+    }
+
+    const pseudoRoot = addPseudoRoot(revisions, revMap);
+
+    // Setup extra fields.
+    let branchCount = 0;
+    for (const rev of revisions) {
+      rev.rank = 1;
+      rev.hasBranch = rev.children.length > 1;
+      rev.isBranchHandled = false;
+
+      if (rev.hasBranch) {
+        branchCount++;
+      }
+    }
+
+    // Make the revisions partially ordered.
+    fixRank(pseudoRoot, revMap);
+    sortByRank(revisions);
+
+    if (branchCount < 8) {
+      // Perform only if the stack is simple enough.
+      splitBranches(revisions, revMap, branchCount);
+    }
+
+    this.revisions = revisions.filter(rev => rev != pseudoRoot);
+  },
+
+  // Calculate a graph edges.
+  //
+  // The graph uses X-axis as index.
+  //
+  //    0 1 2
+  //
+  //    o
+  //    |
+  //    |\
+  //    | |
+  //    | o
+  //    | |
+  //    | | o
+  //    | | |
+  //    | |/
+  //    | o
+  //    | |\
+  //    | | |
+  //    | | o
+  //    | | |
+  //    | o |
+  //    | |/
+  //    o |
+  //    | |
+  //    |/
+  //    |
+  //    o
+  //
+  // Each revision has the following information:
+  //
+  //   * The node's index
+  //   * List of parent node's index
+  //   * Whether the node has children
+  //   * List of other edge, represented by from/to indices
+  //
+  // That correspnds to the following part in the above graph:
+  //
+  //    | | |
+  //    | o |
+  //    | |/
+  //
+  calculateGraph() {
+    if (!this.revisions.length) {
+      return;
+    }
+
+    let revisions = this.revisions;
+    if (!this.showAbandoned) {
+      revisions = revisions.filter(rev => rev.status !== "abandoned");
+    }
+
+    const revMap = {};
+    for (const rev of revisions) {
+      revMap[rev.id] = rev;
+    }
+
+    // A map from edge's index to a set of remaining children.
+    const edges = new Map();
+
+    let maxEdgeIndex = 0;
+    for (const rev of revisions) {
+      const visibleChildren = rev.children.filter(child => child in revMap);
+
+      const graph = {
+        // This node's index.
+        index: null,
+
+        // List of parent node's index.
+        parentIndices: [],
+
+        // True if there are visible children.
+        hasChildren: visibleChildren.length > 0,
+
+        // Othe edges in the same row.
+        otherEdges: [],
+      };
+      rev.graph = graph;
+
+      // Find all parents, and clear the parent's edge if this node is the last
+      // remaining child.
+      for (const [index, edge] of edges) {
+        if (edge.remainingChildren.has(rev.id)) {
+          edge.remainingChildren.delete(rev.id);
+          if (edge.remainingChildren.size == 0) {
+            edges.delete(index);
+          }
+
+          graph.parentIndices.push(index);
+        }
+      }
+
+      // After cleaning up parent edges, the remaining edges are drawn
+      // concurrently.
+      for (const [index, edge] of edges) {
+        graph.otherEdges.push({
+          from: index,
+          to: index,
+        });
+      }
+
+      // Find this node's index.
+      if (graph.parentIndices.length > 0) {
+        for (const parentIndex of graph.parentIndices) {
+          if (!edges.has(parentIndex)) {
+            // If there's any parent where this node is the last remaining
+            // child, use that parent's index.
+            //
+            //   o <- this node
+            //   |
+            //   o <- parent
+            //   |
+            //
+            graph.index = parentIndex;
+            break;
+          }
+        }
+
+        if (graph.index === null) {
+          // If all parents still have remaining children, put this node
+          // at the first parent's index, and move the remaining children of
+          // the first child into a new index.
+          //
+          // | <--- edge for remaining chidlren
+          // |
+          // | o <- this node
+          //  \|
+          //   |
+          //   o <- parent
+          //   |
+          //
+          const parentIndex = graph.parentIndices[0];
+          graph.index = parentIndex;
+          const newParentIndex = findFreeIndex(edges);
+          const parentEdge = edges.get(parentIndex);
+          edges.delete(parentIndex);
+          edges.set(newParentIndex, parentEdge);
+
+          // Redirect the edge for the remaining children.
+          for (const edge of graph.otherEdges) {
+            if (edge.from === parentIndex) {
+              edge.to = newParentIndex;
+              break;
+            }
+          }
+        }
+      } else {
+        // This node has no parent. Put it into a new index.
+        graph.index = findFreeIndex(edges);
+      }
+
+      // Put a new edge to this node's children.
+      if (graph.hasChildren) {
+        edges.set(graph.index, {
+          remainingChildren: new Set(visibleChildren),
+        });
+      }
+
+      maxEdgeIndex = Math.max(maxEdgeIndex, graph.index);
+    }
+
+    this.maxEdgeIndex = maxEdgeIndex;
+  },
+
+  svgWidth() {
+    return (this.maxEdgeIndex + 2) * EdgeMargin;
+  },
+
+  toNodeX(index) {
+    return EdgeMargin * (1 + index);
+  },
+
+  // Draw graph in the pre-populated svg elements for each revision's row.
+  drawGraph() {
+    const color = "#cc0099";
+
+    for (const rev of this.revisions) {
+      if (!this.showAbandoned) {
+        if (rev.status === "abandoned") {
+          continue;
+        }
+      }
+
+      const svg = this.svgs.get(rev.id);
+
+      // Cleanup the previous result before switching showAbandoned checkbox.
+      svg.setAttribute("width", this.svgWidth());
+      svg.replaceChildren();
+
+      // Fit to the table row.
+      // The height of the row can differ for each, due to:
+      //   * The number of reviewers can be different for each
+      //   * Long title can be wrapped into multiple lines
+      const h = svg.parentNode.offsetHeight;
+      svg.setAttribute("height", h);
+
+      const graph = rev.graph;
+
+      const x = this.toNodeX(graph.index);
+      const y = h / 2;
+
+      // An edge to children.
+      if (graph.hasChildren) {
+        const path = document.createElementNS(svgns, "path");
+        svg.append(path);
+        path.setAttribute("d", `M ${x} ${y} L ${x} 0`);
+        path.setAttribute("fill", "none");
+        path.setAttribute("stroke", color);
+        path.setAttribute("stroke-width", "1");
+      }
+
+      // An edge from parents.
+      for (const index of graph.parentIndices) {
+        const px = this.toNodeX(index);
+
+        const path = document.createElementNS(svgns, "path");
+        svg.append(path);
+        path.setAttribute("d",
+                          `M ${x} ${y} ` +
+                          `C ${x} ${h},  ${px} ${y},  ${px} ${h}`);
+        path.setAttribute("fill", "none");
+        path.setAttribute("stroke", color);
+        path.setAttribute("stroke-width", "1");
+      }
+
+      // Other edges in the same row.
+      for (const {from, to} of graph.otherEdges) {
+        const fromX = this.toNodeX(from);
+        const toX = this.toNodeX(to);
+
+        const path = document.createElementNS(svgns, "path");
+        svg.append(path);
+        path.setAttribute("d",
+                          `M ${toX} 0 ` +
+                          `L ${toX} ${y} ` +
+                          `C ${toX} ${h}, ${fromX} ${y}, ${fromX} ${h}`);
+        path.setAttribute("fill", "none");
+        path.setAttribute("stroke", color);
+        path.setAttribute("stroke-width", "1");
+      }
+
+      // This node.
+      const circle = document.createElementNS(svgns, "circle");
+      svg.append(circle);
+      circle.setAttribute("fill", color);
+      circle.setAttribute("stroke", color);
+      circle.setAttribute("cx", x);
+      circle.setAttribute("cy", y);
+      circle.setAttribute("r", "3");
+    }
+  },
+
+  createTable() {
+    const phabUrl = document.querySelector(".phabricator-revisions").getAttribute("data-phabricator-base-uri");
+
+    const tbody = document.querySelector("tbody.phabricator-revision");
+
+    let hasAbandonedRevisions = false;
+
+    for (const rev of this.revisions.slice().reverse()) {
+      const trRevision = document.createElement("tr");
+      this.trs.set(rev.id, trRevision);
+
+      // Graph
+
+      const tdGraph = document.createElement("td");
+      tdGraph.style.paddingTop = "0";
+      tdGraph.style.paddingBottom = "0";
+
+      const svg = document.createElementNS(svgns, "svg");
+      svg.setAttribute("xmlns", svgns);
+      svg.setAttribute("version", "1.1");
+      svg.setAttribute("width", this.svgWidth());
+      svg.setAttribute("height", 10);
+      svg.style.display = "block";
+      this.svgs.set(rev.id,  svg);
+      tdGraph.append(svg);
+
+      // Revision ID
+
+      const tdId = document.createElement("td");
+      const revLink = document.createElement("a");
+      revLink.setAttribute("href", phabUrl + rev.id);
+      revLink.append(rev.id);
+      tdId.append(revLink);
+
+      // Revision status
+
+      const tdRevisionStatus = document.createElement("td");
+
+      const spanRevisionStatus = document.createElement("span");
+      spanRevisionStatus.classList.add("revision-status-box-" + rev.status);
+
+      const spanRevisionStatusIcon = document.createElement("span");
+      spanRevisionStatusIcon.classList.add("revision-status-icon-" + rev.status);
+      spanRevisionStatus.append(spanRevisionStatusIcon);
+
+      const spanRevisionStatusText = document.createElement("span");
+      spanRevisionStatusText.append(rev.long_status);
+      spanRevisionStatus.append(spanRevisionStatusText);
+
+      tdRevisionStatus.append(spanRevisionStatus);
+
+      // Reviewers
+
+      const tdReviewers = document.createElement("td");
+
+      const tableReviews = document.createElement("table");
+      tableReviews.classList.add("phabricator-reviewers");
+
+      for (const review of rev.reviews.slice().sort((a, b) => {
+        return a.user < b.user ? -1 : 1;
+      })) {
+        const trReview = document.createElement("tr");
+        trReview.title = review.long_status;
+
+        const tdReviewStatus = document.createElement("td");
+        const spanReviewStatusIcon = document.createElement("span");
+        spanReviewStatusIcon.classList.add("review-status-icon-" + review.status);
+        tdReviewStatus.append(spanReviewStatusIcon);
+
+        const tdReviewer = document.createElement("td");
+        tdReviewer.classList.add("review-reviewer");
+        tdReviewer.append(review.user);
+
+        trReview.append(tdReviewStatus, tdReviewer);
+
+        tableReviews.append(trReview);
+      }
+      tdReviewers.append(tableReviews);
+
+      // Revision Title
+
+      const tdTitle = document.createElement("td");
+      tdTitle.classList.add("phabricator-title");
+      tdTitle.append(rev.title);
+
+      // Hide abandoned revisions
+
+      if (rev.status === "abandoned") {
+        if (!this.showAbandoned) {
+          trRevision.classList.add("bz_default_hidden");
+        }
+        hasAbandonedRevisions = true;
+      }
+
+      trRevision.append(
+        tdGraph,
+        tdId,
+        tdRevisionStatus,
+        tdReviewers,
+        tdTitle
+      );
+
+      tbody.append(trRevision);
+    }
+
+    if (hasAbandonedRevisions) {
+      document.querySelector("tbody.phabricator-show-abandoned").classList.remove("bz_default_hidden");
+    }
+  },
+
+  // Show/hide abandoned revisions.
+  updateVisibility() {
+    for (const rev of this.revisions) {
+      const tr = this.trs.get(rev.id);
+
+      if (rev.status !== "abandoned") {
+        continue;
+      }
+
+      tr.classList.toggle("bz_default_hidden", !this.showAbandoned);
+    }
+  },
+
+  async onLoad() {
+    const showAbandonedCheckbox = document.querySelector("#phabricator-show-abandoned");
+    this.showAbandoned = showAbandonedCheckbox.checked;
+
+    const tbody = document.querySelector("tbody.phabricator-revision");
 
     function displayLoadError(errStr) {
-        var errRow = tbody.find('.phabricator-loading-error-row');
-        errRow.find('.phabricator-load-error-string').text(errStr);
-        errRow.removeClass('bz_default_hidden');
+      const errRow = tbody.querySelector(".phabricator-loading-error-row");
+      errRow.querySelector(".phabricator-load-error-string").replaceChildren(errStr);
+      errRow.classList.remove("bz_default_hidden");
     }
 
     try {
-        const { revisions } = await Bugzilla.API.get(`phabbugz/bug_revisions/${BUGZILLA.bug_id}`);
+      const { revisions } = await Bugzilla.API.get(`phabbugz/bug_revisions/${BUGZILLA.bug_id}`);
 
-        if (revisions.length) {
-            sortRevisions(revisions).forEach(rev => tbody.append(revisionRow(rev)));
-        } else {
-            displayLoadError('none returned from server');
-        }
-    } catch ({ message }) {
-        displayLoadError(message);
+      if (revisions.length) {
+        this.setRevisions(revisions);
+        this.calculateGraph();
+        this.createTable();
+        this.drawGraph();
+      } else {
+        displayLoadError("none returned from server");
+      }
+    } catch (e) {
+      console.error(e);
+      displayLoadError(e.message);
     }
 
-    tbody.find('.phabricator-loading-row').addClass('bz_default_hidden');
+    tbody.querySelector(".phabricator-loading-row").classList.add("bz_default_hidden");
+
+    showAbandonedCheckbox.addEventListener("click", event => {
+      this.showAbandoned = showAbandonedCheckbox.checked;
+      this.updateVisibility();
+      this.calculateGraph();
+      this.drawGraph();
+    });
+  },
 };
 
-$().ready(function() {
-    Phabricator.getBugRevisions();
-
-    $('#phabricator-show-abandoned').on('click', function (event) {
-        $('tbody.phabricator-revision > tr').each(function() {
-            var row = $(this);
-            if (row.attr('data-status') === 'abandoned') {
-                if ($('#phabricator-show-abandoned').prop('checked') == true) {
-                    row.removeClass('bz_default_hidden');
-                }
-                else {
-                    row.addClass('bz_default_hidden');
-                }
-            }
-        });
-    });
+window.addEventListener("DOMContentLoaded", function() {
+  Phabricator.onLoad();
 });
