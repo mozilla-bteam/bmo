72eed3b428fa0c8c3dcba7eb1501033a26d829a9
David Lawrence <dkl@mozilla.com>
2022-10-05 18:53:05 -0400

    Bug 1792986 - Add Github API endpoint to be used for automatically linking PRs to bug attachments (debugging)

--- Bugzilla/API/V1/Github.pm
+++ Bugzilla/API/V1/Github.pm
@@ -118,9 +118,11 @@ sub pull_request {
 sub verify_signature {
   my ($self)             = @_;
   my $payload            = $self->req->body;
+  DEBUG($payload);
   my $secret             = Bugzilla->params->{github_pr_signature_secret};
   my $received_signature = $self->req->headers->header('X-Hub-Signature-256');
   my $expected_signature = 'sha256=' . hmac_sha256_hex($secret, $payload);
+  DEBUG("expected: $expected_signature, received: $received_signature");
   return secure_compare($expected_signature, $received_signature) ? 1 : 0;
 }
 
