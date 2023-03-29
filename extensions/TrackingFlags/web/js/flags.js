501ccbfdef453f0c950c61c56df91bd6777f67c8
David Lawrence <dkl@mozilla.com>
2023-03-29 16:53:33 -0400

    * Fixed formatting for flags.js * Updated bug_modal template to properly hide flags with out a value set

--- extensions/TrackingFlags/web/js/flags.js
+++ extensions/TrackingFlags/web/js/flags.js
@@ -7,60 +7,56 @@
  */
 
 $(function() {
-  'use strict';
+    'use strict';
 
-   // tracking flags
-   $('.tracking-flags select')
-     .change(function(event) {
-       tracking_flag_change(event.target);
-     });
+    // tracking flags
+    $('.tracking-flags select')
+        .change(function(event) {
+            tracking_flag_change(event.target);
+        });
 
-  function tracking_flag_change(e) {
-    var value = e.value;
-    var prefill;
-    if (TrackingFlags.comments[e.name])
-      prefill = TrackingFlags.comments[e.name][e.value];
-    if (!prefill) {
-      var cr = document.getElementById('cr_' + e.id);
-      if (cr)
-        cr.parentElement.removeChild(cr);
-      return;
+    function tracking_flag_change(e) {
+        var value = e.value;
+        var prefill;
+        if (TrackingFlags.comments[e.name])
+            prefill = TrackingFlags.comments[e.name][e.value];
+        if (!prefill) {
+            var cr = document.getElementById('cr_' + e.id);
+            if (cr) cr.parentElement.removeChild(cr);
+            return;
+        }
+        if (!document.getElementById('cr_' + e.id)) {
+            // create "comment required"
+            var span = document.createElement('span');
+            span.id = 'cr_' + e.id;
+            span.appendChild(document.createTextNode(' ('));
+            var a = document.createElement('a');
+            a.appendChild(document.createTextNode('comment required'));
+            a.href = '#';
+            a.onclick = function(event) {
+                event.preventDefault();
+                var c = document.getElementById('comment');
+                c.focus();
+                c.select();
+                var btn = document.getElementById('add_comment') || document.getElementById('add-comment');
+                if (btn) btn.scrollIntoView();
+            };
+            span.appendChild(a);
+            span.appendChild(document.createTextNode(')'));
+            e.parentNode.appendChild(span);
+        }
+        // prefill comment
+        var commentEl = document.getElementById('comment');
+        if (!commentEl) return;
+        var value = commentEl.value;
+        if (value == prefill) return;
+        if (value == '') {
+            commentEl.value = prefill;
+            a.innerHTML = 'comment required';
+        } else {
+            commentEl.value = prefill + "\n\n" + value;
+            a.innerHTML = 'comment updated';
+        }
     }
-    if (!document.getElementById('cr_' + e.id)) {
-      // create "comment required"
-      var span = document.createElement('span');
-      span.id = 'cr_' + e.id;
-      span.appendChild(document.createTextNode(' ('));
-      var a = document.createElement('a');
-      a.appendChild(document.createTextNode('comment required'));
-      a.href = '#';
-      a.onclick = function(event) {
-        event.preventDefault();
-        var c = document.getElementById('comment');
-        c.focus();
-        c.select();
-        var btn = document.getElementById('add_comment') || document.getElementById('add-comment');
-        if (btn)
-          btn.scrollIntoView();
-      };
-      span.appendChild(a);
-      span.appendChild(document.createTextNode(')'));
-      e.parentNode.appendChild(span);
-    }
-    // prefill comment
-    var commentEl = document.getElementById('comment');
-    if (!commentEl)
-      return;
-    var value = commentEl.value;
-    if (value == prefill)
-      return;
-    if (value == '') {
-      commentEl.value = prefill;
-      a.innerHTML = 'comment required';
-    } else {
-      commentEl.value = prefill + "\n\n" + value;
-      a.innerHTML = 'comment updated';
-    }
-  }
 });
 
