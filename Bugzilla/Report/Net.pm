12950dda7240d2464e361479f9d26b61babe3715
David Lawrence <dklawren@users.noreply.github.com>
2024-02-22 13:54:27 -0500

    Bug 1877578 - Update sitemap extension and mining chart code to upload data to google cloud storage instead of aws s3

--- Bugzilla/Report/Net.pm
+++ Bugzilla/Report/Net.pm
@@ -13,7 +13,6 @@ use Moo;
 use Bugzilla;
 use Bugzilla::Error;
 use Bugzilla::Net::Google;
-use Bugzilla::Net::S3;
 
 has driver     => (is => 'lazy');
 has is_enabled => (is => 'lazy');
@@ -21,26 +20,25 @@ has is_enabled => (is => 'lazy');
 sub _build_driver {
   my $self = shift;
 
-  my $driver = Bugzilla::Net::S3->new({
-    client_id  => Bugzilla->params->{s3_mining_access_key_id},
-    secret_key => Bugzilla->params->{s3_mining_secret_access_key},
-    bucket     => Bugzilla->params->{s3_mining_bucket},
-    host       => Bugzilla->params->{aws_host},
-    secure     => 1,
-    retry      => 1,
+  my $driver = Bugzilla::Net::Google->new({
+    bucket          => Bugzilla->params->{mining_google_bucket},
+    host            => Bugzilla->params->{mining_google_host},
+    service_account => Bugzilla->params->{mining_google_service_account},
+    secure          => 1,
+    retry           => 1,
   });
 
   return $driver;
 }
 
 sub _build_is_enabled {
-  return Bugzilla->params->{s3_mining_enabled} ? 1 : 0;
+  return Bugzilla->params->{mining_enabled} ? 1 : 0;
 }
 
 sub set_data {
   my ($self, $product, $data) = @_;
   unless ($self->driver->add_key($product, $data)) {
-    warn "Failed to add data for product $product to S3: "
+    warn "Failed to add data for product $product to net storage: "
       . $self->driver->error_string . "\n";
     ThrowCodeError('net_mining_add_failed',
       {product => $product, reason => $self->driver->error_string});
@@ -52,7 +50,7 @@ sub get_data {
   my ($self, $product) = @_;
   my $data = $self->driver->get_key($product);
   if (!$data) {
-    warn "Failed to retrieve data for product $product from S3: "
+    warn "Failed to retrieve data for product $product from net storage: "
       . $self->driver->error_string . "\n";
     ThrowCodeError('net_mining_get_failed',
       {product => $product, reason => $self->driver->error_string});
@@ -63,7 +61,7 @@ sub get_data {
 sub remove_data {
   my ($self, $product) = @_;
   $self->driver->delete_key($product)
-    or warn "Failed to remove data for product $product from S3: "
+    or warn "Failed to remove data for product $product from net storage: "
     . $self->driver->error_string . "\n";
   return $self;
 }
