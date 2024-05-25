c621c6a2c3ef2c6db8399fb0677c3db4b0fb0482
David Lawrence <dkl@mozilla.com>
2024-05-24 20:26:36 -0400

    Moved ComponentGraveyard to Controller directory

--- Bugzilla/App/Controller/BMO/ComponentGraveyard.pm
+++ Bugzilla/App/Controller/BMO/ComponentGraveyard.pm
@@ -5,7 +5,7 @@
 # This Source Code Form is "Incompatible With Secondary Licenses", as
 # defined by the Mozilla Public License, v. 2.0.
 
-package Bugzilla::App::BMO::ComponentGraveyard;
+package Bugzilla::App::Controller::BMO::ComponentGraveyard;
 use Mojo::Base 'Mojolicious::Controller';
 
 use Bugzilla;
