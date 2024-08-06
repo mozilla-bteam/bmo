2a32a0f2be7d865e01f8524f28ba9fd21f8fb72e
David Lawrence <dkl@mozilla.com>
2024-08-06 14:44:32 -0400

    Added functionality to cron script to remove a needinfo if the comment was marked as spam

--- extensions/AntiSpam/bin/spam_cleanup.pl
+++ extensions/AntiSpam/bin/spam_cleanup.pl
@@ -15,8 +15,52 @@ use lib qw(. lib local/lib/perl5);
 use Bugzilla;
 BEGIN { Bugzilla->extensions() }
 
+use Bugzilla::Comment;
 use Bugzilla::Constants;
+use Bugzilla::User;
+
+use List::Util qw(none);
 
 Bugzilla->usage_mode(USAGE_MODE_CMDLINE);
 
+# Set current user to automation
+my $auto_user = Bugzilla::User->check({name => 'github-automation@bmo.tld'});
+$auto_user->{groups}       = [Bugzilla::Group->get_all];
+$auto_user->{bless_groups} = [Bugzilla::Group->get_all];
+Bugzilla->set_user($auto_user);
 
+# Find all spam comment entries made before a specific amount of time
+my $cutoff_time = DateTime->now(time_zone => 'UTC')->subtract(minutes => 30)
+  ->strftime('%Y-%m-%d %T');
+
+my $dbh = Bugzilla->dbh;
+
+my $query = q{
+  SELECT comment_id 
+    FROM antispam_comment_cleanup
+   WHERE comment_ts <= ?
+  ORDER BY comment_id};
+my $comment_ids = $dbh->selectcol_arrayref($query, undef, $cutoff_time);
+
+foreach my $comment_id (@{$comment_ids}) {
+  my $comment = Bugzilla::Comment->new($comment_id);
+  my $bug     = $comment->bug;
+
+  # Clear a needinfo that was set on a comment tagged as spam
+  my @clear_flags = ();
+  foreach my $flag (@{$bug->flags}) {
+    next
+      if $flag->type->name ne 'needinfo'
+      && $flag->status ne '?'
+      && $flag->requestee_id != $comment->author->id
+      && $flag->modification_date eq $comment->creation_ts;
+    push @clear_flags, {id => $flag->id, status => 'X'};
+  }
+  $bug->set_flags(\@clear_flags, []) if @clear_flags;
+
+  $bug->update;
+
+  # Lastly we need to remove the entry from the cleanup queue
+  $dbh->do('DELETE FROM antispam_comment_cleanup WHERE comment_id = ?',
+    undef, $comment_id);
+}
