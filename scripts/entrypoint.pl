b51c5623ea015b8031ce46581d5d5e671dc2a670
David Lawrence <dkl@mozilla.com>
2024-11-19 12:33:21 -0500

    CircleCI fixes

--- scripts/entrypoint.pl
+++ scripts/entrypoint.pl
@@ -104,7 +104,7 @@ sub cmd_selenium_dev {
   cmd_load_test_data();
   check_data_dir();
   copy_qa_extension();
-  mkdir('/app/artifacts');
+  mkdir '/app/artifacts' if !-d '/app/artifacts';
 
   assert_database->get();
   my $httpd_exit_f = run_cereal_and_httpd('-DACCESS_LOGS');
@@ -179,7 +179,7 @@ sub cmd_test_qa {
 
   cmd_load_test_data();
   check_data_dir();
-  mkdir('/app/artifacts');
+  mkdir '/app/artifacts' if !-d '/app/artifacts';
 
   assert_database()->get;
   my $httpd_exit_f = run_cereal_and_httpd('-DHTTPD_IN_SUBDIR', '-DACCESS_LOGS');
