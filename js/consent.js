bfa91f5893d6f00ac9f1b5c73a37fd83a5e7797f
David Lawrence <dkl@mozilla.com>
2024-11-25 18:11:12 -0500

    Bug 1879792 - Add a cookie banner to BMO

---
+++ js/consent.js
@@ -0,0 +1,209 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+var MozConsentBanner = {};
+var EVENT_NAME_STATUS = 'mozConsentStatus';
+var EVENT_NAME_OPEN = 'mozConsentOpen';
+var EVENT_NAME_CLOSE = 'mozConsentClose';
+var COOKIE_ID = 'moz-consent-pref';
+var COOKIE_EXPIRY_DAYS = 182; // 6 months
+var MSG_OPTIONS_ERROR_DISABLED = 'verifyOptions(): cookies are disabled.';
+var MSG_OPTIONS_ERROR_EXPIRY = 'verifyOptions(): options.cookieExpiryDays not set.';
+var MSG_OPTIONS_ERROR_HELPER = 'verifyOptions(): options.helper not set.';
+var MSG_OPTIONS_ERROR_OPT_OUT = 'verifyOptions(): options.optOut not set.';
+var MSG_OPTIONS_VALID = 'verifyOptions(): success.';
+var options;
+
+/**
+ * Sets cookie indicating consent choice.
+ * @param {Object} data - consent settings.
+ */
+MozConsentBanner.setConsentCookie = function (value) {
+  try {
+    var date = new Date();
+    date.setDate(date.getDate() + options.cookieExpiryDays);
+    var expires = date.toUTCString();
+    options.helper.setItem(options.cookieID, (value ? 'yes' : 'no'),
+      expires, '/', options.cookieDomain, false, 'lax');
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+/**
+ * Removes cookie indicating consent choice.
+ */
+MozConsentBanner.clearConsentCookie = function () {
+  return  options.helper.removeItem(options.cookieID, '/', options.cookieDomain, false, 'lax');
+};
+
+/**
+ * Get consent cookie value if set.
+ * @returns {Boolean}
+ */
+MozConsentBanner.getConsentCookie = function () {
+  try {
+    return options.helper.hasItem(options.cookieID) && options.helper.getItem(options.cookieID);
+  } catch (e) {
+    return false;
+  }
+};
+
+/**
+ * Helper for dispatching a custom event with a
+ * given name and data payload.
+ * @param {Object} eventName
+ * @param {Object} eventData
+ */
+MozConsentBanner.dispatchEvent = function (eventName, eventData) {
+  if (typeof window.CustomEvent === 'function') {
+    // Modern browsers
+    window.dispatchEvent(new CustomEvent(eventName, {
+      detail: eventData
+    }));
+  } else if (typeof document.createEvent === 'function') {
+    // Internet Explorer 10
+    var customEvent = document.createEvent('CustomEvent');
+    customEvent.initCustomEvent(eventName, false, false, eventData);
+    window.dispatchEvent(customEvent);
+  } else if (typeof document.createEventObject === 'function') {
+    // Internet Explorer 9
+    var customEventObj = document.createEventObject();
+    customEventObj.type = eventName;
+    customEventObj.bubbles = false;
+    customEventObj.cancelable = false;
+    customEventObj.detail = eventData; // Optional data for the event
+    window.fireEvent('on' + customEventObj.type, customEventObj);
+  }
+};
+
+/**
+ * Event handler for accepting cookies.
+ */
+MozConsentBanner.onAcceptClick = function () {
+  MozConsentBanner.setConsentCookie(true);
+  MozConsentBanner.dispatchEvent(EVENT_NAME_STATUS, true);
+  MozConsentBanner.dispatchEvent(EVENT_NAME_CLOSE, {});
+};
+
+/**
+ * Event handler for rejecting cookies.
+ */
+MozConsentBanner.onRejectClick = function () {
+  MozConsentBanner.setConsentCookie(false);
+  MozConsentBanner.dispatchEvent(EVENT_NAME_STATUS, false);
+  MozConsentBanner.dispatchEvent(EVENT_NAME_CLOSE, {});
+};
+
+MozConsentBanner.onClearClick = function () {
+  MozConsentBanner.clearConsentCookie();
+  MozConsentBanner.dispatchEvent(EVENT_NAME_STATUS, false);
+  MozConsentBanner.dispatchEvent(EVENT_NAME_OPEN, {});
+}
+
+/**
+ * Validates BannerOptions configuration object.
+ * @param {BannerOptions} object containing one or more properties.
+ * @returns {String} message to indicate either success or error.
+ */
+MozConsentBanner.verifyOptions = function (options) {
+  // Verify cookie expiry has been set.
+  if (typeof options.cookieExpiryDays !== 'number') {
+    return MSG_OPTIONS_ERROR_EXPIRY;
+  }
+
+  // Verify cookie helper dependency is defined.
+  if (!options.helper || typeof options.helper.setItem !== 'function') {
+    return MSG_OPTIONS_ERROR_HELPER;
+  }
+
+  // Verify that optOut flag is set.
+  if (typeof options.optOut !== 'boolean') {
+    return MSG_OPTIONS_ERROR_OPT_OUT;
+  }
+
+  // Verify that cookies are enabled in the browser.
+  if (!options.helper.enabled()) {
+    return MSG_OPTIONS_ERROR_DISABLED;
+  }
+  return MSG_OPTIONS_VALID;
+};
+
+/**
+ * MozConsentBanner.init() configuration options:
+ * @typedef {Object} BannerOptions
+ * @property {string} cookieDomain - Sets a consent cookie for a specific host name.
+ * @property {number} cookieExpiryDays - Consent cookie expiry by number of days.
+ * @property {string} cookieID - Consent cookie identifier.
+ * @property {object} helper (required) - reference to @mozmeao/cookie-helper peer dependency.
+ * @property {object} optOut - Sets the banner to be opt-out instead of the default opt-in.
+ */
+
+/**
+ * Initializes cookie banner and binds events.
+ * @param {BannerOptions} object containing one or more properties.
+ * @returns {Boolean} to indicate if initialization succeeded.
+ */
+MozConsentBanner.init = function (config) {
+  /**
+   * Default configuration options that can be overridden via init().
+   */
+  var defaults = {
+    cookieDomain: null,
+    cookieExpiryDays: COOKIE_EXPIRY_DAYS,
+    cookieID: COOKIE_ID,
+    optOut: false,
+    eventName: EVENT_NAME_STATUS,
+    helper: null
+  };
+  for (var opt in config) {
+    if (Object.prototype.hasOwnProperty.call(config, opt)) {
+      defaults[opt] = config[opt];
+    }
+  }
+  options = defaults;
+
+  /**
+   * If domain for consent cookie has not be supplied via config,
+   * set it via default BUGZILLA urlbase
+   */
+  if (!options.cookieDomain) {
+    const url = new URL(BUGZILLA.config.urlbase);
+    options.cookieDomain = url.hostname;
+  }
+
+  /**
+   * Validate options object before trying to show the banner.
+   */
+  var configValidationMsg = MozConsentBanner.verifyOptions(options);
+  if (configValidationMsg !== MSG_OPTIONS_VALID) {
+    if (window.console && window.console.error) {
+      console.error(configValidationMsg); // eslint-disable-line no-console
+    }
+    return false;
+  }
+
+  /**
+   * If cookie exists and consent has previously been given,
+   * despatch consent event. Otherwise show the banner.
+   */
+  var consent = MozConsentBanner.getConsentCookie();
+  if (consent) {
+    MozConsentBanner.dispatchEvent(EVENT_NAME_STATUS, consent);
+  } else {
+    MozConsentBanner.dispatchEvent(EVENT_NAME_OPEN, {});
+
+    /**
+     * If the banner is opt out, despatch event to indicate
+     * cookies and analytics are OK to load.
+     */
+    if (options.optOut) {
+      MozConsentBanner.dispatchEvent(EVENT_NAME_STATUS, true);
+    }
+  }
+  return true;
+};
---
+++ qa/t/1_test_cookie_consent.t
@@ -0,0 +1,119 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This Source Code Form is 'Incompatible With Secondary Licenses', as
+# defined by the Mozilla Public License, v. 2.0.
+
+use strict;
+use warnings;
+use lib qw(lib ../../lib ../../local/lib/perl5);
+
+use Test::More 'no_plan';
+
+use List::Util qw(first none);
+use QA::Util;
+
+my ($sel, $config) = get_selenium();
+
+# Turn on cookie consent
+log_in($sel, $config, 'admin');
+set_parameters($sel,
+  {'Administrative Policies' => {'cookie_consent_enabled-on' => undef}});
+
+# Accept all cookies for admin user
+$sel->click_ok('moz-consent-banner-button-accept');
+
+# Make sure that a 'moz-consent-pref' cookie was set to yes
+my $cookies     = $sel->driver->get_all_cookies;
+my $pref_cookie = first { $_->{name} eq 'moz-consent-pref' } @{$cookies};
+ok($pref_cookie && $pref_cookie->{value} eq 'yes',
+  'Consent cookie set to yes properly');
+
+# Create a test bug
+file_bug_in_product($sel, 'TestProduct');
+my $bug_summary = 'Cookie consent test bug';
+$sel->type_ok('short_desc', $bug_summary);
+my $bug1_id = create_bug($sel, $bug_summary);
+
+# Run a buglist query to set a cookie such as LASTORDER which is non-essential
+open_advanced_search_page($sel);
+$sel->type_ok('short_desc', 'Cookie consent test bug');
+$sel->select_ok('order', 'label=Bug Number', 'Select order by bug number');
+$sel->click_ok('Search');
+$sel->wait_for_page_to_load_ok(WAIT_TIME);
+$sel->title_is('Bug List');
+$sel->is_text_present_ok('One bug found');
+$sel->is_text_present_ok('Cookie consent test bug');
+
+$cookies = $sel->driver->get_all_cookies;
+my $last_order_cookie = first { $_->{name} eq 'LASTORDER' } @{$cookies};
+ok($last_order_cookie, 'Last order cookie set properly');
+
+# Under user preferences, change cookie preferences to reject
+$sel->click_ok('header-account-menu-button');
+$sel->click_ok('link=Preferences');
+$sel->wait_for_page_to_load(WAIT_TIME);
+$sel->title_is('User Preferences');
+$sel->click_ok('link=Account');
+$sel->wait_for_page_to_load_ok(WAIT_TIME);
+$sel->title_is('User Preferences');
+$sel->uncheck_ok('cookie_consent');
+$sel->click_ok('update');
+$sel->wait_for_page_to_load(WAIT_TIME);
+$sel->title_is('User Preferences');
+
+# Clear all cookies except the consent cookie and login stuff
+$cookies = $sel->driver->get_all_cookies;
+foreach my $cookie (@{$cookies}) {
+  my $name = $cookie->{name};
+  unless ($name eq 'Bugzilla_login'
+    || $name eq 'Bugzilla_logincookie'
+    || $name eq 'moz-consent-pref')
+  {
+    $sel->driver->delete_cookie_named($name);
+  }
+}
+
+# Verify that LASTORDER and COLUMNLIST are no longer set
+open_advanced_search_page($sel);
+$sel->type_ok('short_desc', 'Cookie consent test bug');
+$sel->select_ok('order', 'label=Bug Number', 'Select order by bug number');
+$sel->click_ok('Search');
+$sel->wait_for_page_to_load_ok(WAIT_TIME);
+$sel->title_is('Bug List');
+$sel->is_text_present_ok('One bug found');
+$sel->is_text_present_ok('Cookie consent test bug');
+
+$cookies = $sel->driver->get_all_cookies;
+$last_order_cookie = first { $_->{name} eq 'LASTORDER' } @{$cookies};
+ok(!$last_order_cookie, 'Last order cookie not set properly');
+
+# Logout and clear all cookies. Then we will reject all cookies and verify
+logout($sel);
+$sel->driver->delete_all_cookies();
+$sel->open_ok('/home', 'Go to home page');
+$sel->click_ok('moz-consent-banner-button-reject');
+
+log_in($sel, $config, 'admin');
+open_advanced_search_page($sel);
+$sel->type_ok('short_desc', 'Cookie consent test bug');
+$sel->select_ok('order', 'label=Bug Number', 'Select order by bug number');
+$sel->click_ok('Search');
+$sel->wait_for_page_to_load_ok(WAIT_TIME);
+$sel->title_is('Bug List');
+$sel->is_text_present_ok('One bug found');
+$sel->is_text_present_ok('Cookie consent test bug');
+
+$cookies = $sel->driver->get_all_cookies;
+$last_order_cookie = first { $_->{name} eq 'LASTORDER' } @{$cookies};
+ok(!$last_order_cookie, 'Last order cookie not set properly');
+
+## Turn off cookie consent
+set_parameters($sel,
+  {'Administrative Policies' => {'cookie_consent_enabled-off' => undef}});
+logout($sel);
+
+done_testing;
+
+1;
