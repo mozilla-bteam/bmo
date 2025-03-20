f8d2cf2eba5dc984963a2d6bf3d735e7353d3a8f
David Lawrence <dkl@mozilla.com>
2025-03-20 18:42:13 -0400

    Bug 1954833 - Change product name from "Fenix" to "Firefox for Android"

--- Bugzilla/Search.pm
+++ Bugzilla/Search.pm
@@ -108,7 +108,9 @@ use Time::HiRes qw(gettimeofday tv_interval);
 #############
 
 # BMO - product aliases for searching
-use constant PRODUCT_ALIASES => {};
+use constant PRODUCT_ALIASES => {
+  'Fenix' => 'Firefox for Android',
+};
 
 # When doing searches, NULL datetimes are treated as this date.
 use constant EMPTY_DATETIME => '1970-01-01 00:00:00';
