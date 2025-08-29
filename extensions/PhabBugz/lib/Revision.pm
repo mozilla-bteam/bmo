8cb91d43361adf389743f06e293d4ea8b8d09d23
Connor Sheehan <cosheehan@mozilla.com>
2025-08-29 11:43:55 -0400

    import `encode_json` from `JSON::MaybeXS`

--- extensions/PhabBugz/lib/Revision.pm
+++ extensions/PhabBugz/lib/Revision.pm
@@ -10,6 +10,7 @@ package Bugzilla::Extension::PhabBugz::Revision;
 use 5.10.1;
 use Moo;
 
+use JSON::MaybeXS qw(encode_json);
 use Mojo::JSON qw(true);
 use Scalar::Util qw(blessed weaken);
 use Types::Standard -all;
