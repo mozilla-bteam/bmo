b59448f51fc9a9c3d3d2157e6d516a3d405bd067
Kohei Yoshino <kohei.yoshino@gmail.com>
2026-04-09 23:28:25 -0400

    Bug 2018260 - "Fields You Can Search On" is blocking people from making it through quicksearch.html doc

--- extensions/TrackingFlags/Extension.pm
+++ extensions/TrackingFlags/Extension.pm
@@ -76,6 +76,31 @@ sub page_before_template {
       {group => 'admin', action => 'access', object => 'administrative_pages'});
     admin_edit($vars);
   }
+  elsif ($page eq 'quicksearch.html') {
+    my $field_names = $vars->{'quicksearch_field_names'};
+    my %quicksearch_extra_field_names;
+    my $active_flags = Bugzilla::Extension::TrackingFlags::Flag->match({is_active => 1});
+    my %active_flag_names = map { $_->{name} => 1 } @$active_flags;
+
+    # Move cf_tracking_* and cf_status_* into a separate var with active flag info
+    foreach my $key (keys %$field_names) {
+      my $db_field = $field_names->{$key}->{db_field};
+
+      if ($db_field =~ /^cf_(tracking|status)_/) {
+        $quicksearch_extra_field_names{$key} = {
+          nicknames => $field_names->{$key}->{nicknames},
+          db_field  => $db_field,
+          # A field is active if it’s an alias like `tracking-firefox-release` or if it’s a real
+          # field that’s active in the database
+          active    => $db_field =~ /_[a-z]+$/ || $active_flag_names{$db_field} ? 1 : 0,
+        };
+
+        delete $field_names->{$key};
+      }
+    }
+
+    $vars->{'quicksearch_extra_field_names'} = \%quicksearch_extra_field_names;
+  }
 }
 
 sub template_before_process {
