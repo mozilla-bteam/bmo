6fadea64e2a555d5ffe2fd7fa107eb34f499359e
Xavier L'Hour <xavier.lhour@gmail.com>
2026-07-02 15:31:22 +0200

    Bug 1806896 - Fix flag_state_activity migration and use FK-handling helpers

--- extensions/Review/Extension.pm
+++ extensions/Review/Extension.pm
@@ -867,11 +867,19 @@ sub db_schema_abstract_schema {
 sub install_update_db {
   my $dbh = Bugzilla->dbh;
 
-  # Bug 1806896 - rename flag_state_activity to flag_activity (now in core schema)
-  if ($dbh->bz_table_info('flag_state_activity')
-    && !$dbh->bz_table_info('flag_activity'))
-  {
-    $dbh->do('RENAME TABLE flag_state_activity TO flag_activity');
+  # Bug 1806896 - migrate flag_state_activity to flag_activity (now in core schema)
+  if ($dbh->bz_table_info('flag_state_activity')) {
+    if ($dbh->bz_table_info('flag_activity')) {
+      my ($new_count) = $dbh->selectrow_array('SELECT COUNT(*) FROM flag_activity');
+      $new_count ||= 0;
+      if (!$new_count) {
+        $dbh->bz_drop_table('flag_activity');
+        $dbh->bz_rename_table('flag_state_activity', 'flag_activity');
+      }
+    }
+    else {
+      $dbh->bz_rename_table('flag_state_activity', 'flag_activity');
+    }
   }
 
   $dbh->bz_add_column('products', 'reviewer_required',
